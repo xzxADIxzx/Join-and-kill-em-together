30b1d07ac1e44b447cf960a8f57ee7f56040ffc7
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2023-07-08 10:05:56 +0300

    add image and canvas

--- ui/Utils.cs
+++ ui/Utils.cs
@@ -44,4 +44,36 @@ public class Utils
         return obj;
     }
 
+    public static GameObject Image(string name, Transform parent, float x, float y, float width, float height, Color color)
+    {
+        var obj = Rect(name, parent, x, y, width, height);
+        Component<Image>(obj, image =>
+        {
+            image.sprite = OptionsMenuToManager.Instance.pauseMenu.transform.Find("Continue").gameObject.GetComponent<Image>().sprite;
+            image.type = UnityEngine.UI.Image.Type.Sliced;
+            image.color = color;
+        });
+
+        return obj;
+    }
+
+    public static GameObject Canvas(string name, Transform parent)
+    {
+        var obj = Object(name, parent);
+        Component<Canvas>(obj, canvas =>
+        {
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            canvas.sortingOrder = 1000; // move to the top (ultraimportant)
+        });
+        Component<CanvasScaler>(obj, scaler =>
+        {
+            scaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+            scaler.referenceResolution = new Vector2(1920f, 1080f);
+        });
+        obj.AddComponent<GraphicRaycaster>();
+
+        return obj;
+    }
+
+    #endregion
     #endregion
