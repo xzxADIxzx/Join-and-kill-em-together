e5195bb125705f2548f2e6d05be06750429d57e3
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2023-07-08 10:02:41 +0300

    add plugin

---
+++ Plugin.cs
@@ -0,0 +1,12 @@
+﻿namespace Jaket;
+
+using UMM;
+
+[UKPlugin(GUID, NAME, VERSION, DESC, false, false)]
+public class Plugin : UKMod
+{
+    const string GUID = "xzxADIxzx.Jaket";
+    const string NAME = "Jaket";
+    const string DESC = "Multikill is still in development, so I created my own multiplayer mod for ultrakill.\nAuthor: xzxADIxzx#7729 & Sowler#5518";
+    const string VERSION = "0.0.1";
+}
