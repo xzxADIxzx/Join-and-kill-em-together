3f6a7bb8716ed1a40cefc6051664e17553e4cb2c
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2023-07-09 20:43:05 +0300

    update plugin version

--- Plugin.cs
+++ Plugin.cs
@@ -13,7 +13,7 @@ public class Plugin : UKMod
     const string GUID = "xzxADIxzx.Jaket";
     const string NAME = "Jaket";
     const string DESC = "Multikill is still in development, so I created my own multiplayer mod for ultrakill.\nAuthor: xzxADIxzx#7729 & Sowler#5518";
-    const string VERSION = "0.0.1";
+    const string VERSION = "0.1.0";
 
     public static Plugin Instance;
     public static bool Initialized;
