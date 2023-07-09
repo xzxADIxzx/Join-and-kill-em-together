9112de8efa65d9a32834eec169f712c7c95da91d
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2023-07-09 15:19:06 +0300

    add entity

---
+++ net/Entity.cs
@@ -0,0 +1,39 @@
+namespace Jaket.Net;
+
+using System.IO;
+using UnityEngine;
+
+public abstract class Entity : MonoBehaviour
+{
+    /// <summary> Last update time via snapshots. </summary>
+    public float LastUpdate;
+
+    /// <summary> Writes the entity data to the writer. </summary>
+    public abstract void Write(BinaryWriter w);
+
+    /// <summary> Reads the entity data from the writer. </summary>
+    public abstract void Read(BinaryReader r);
+
+    /// <summary> Class for interpolating floating point values. </summary>
+    public class FloatLerp
+    {
+        /// <summary> Interpolation values. </summary>
+        private float last, target;
+
+        /// <summary> Updates interpolation values. </summary>
+        public void Set(float value)
+        {
+            last = target;
+            target = value;
+        }
+
+        /// <summary> Reads values to be interpolated from the reader. </summary>
+        public void Read(BinaryReader r) => Set(r.ReadSingle());
+
+        /// <summary> Returns an intermediate value. </summary>
+        public float Get(float lastUpdate) => Mathf.Lerp(last, target, (Time.time - lastUpdate) / Networking.SNAPSHOTS_SPACING);
+
+        /// <summary> Returns the intermediate value of the angle. </summary>
+        public float GetAngel(float lastUpdate) => Mathf.LerpAngle(last, target, (Time.time - lastUpdate) / Networking.SNAPSHOTS_SPACING);
+    }
+}
