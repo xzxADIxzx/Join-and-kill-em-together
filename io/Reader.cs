ed1499ca16657603a5a1d6c1765290233cc54172
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2023-07-14 21:05:43 +0300

    add reader & writer

---
+++ io/Reader.cs
@@ -0,0 +1,56 @@
+namespace Jaket.IO;
+
+using Steamworks;
+using System;
+using System.IO;
+using UnityEngine;
+
+/// <summary> Builtin binary reader but with some extra methods for convenience. </summary>
+public class Reader
+{
+    /// <summary> Input for reader. </summary>
+    private BinaryReader r;
+
+    /// <summary> Creates a reader with the given input. </summary>
+    private Reader(BinaryReader r) => this.r = r;
+
+    /// <summary> Current cursor position in the stream. </summary>
+    public long Position { get => r.BaseStream.Position; }
+
+    /// <summary> Stream length. </summary>
+    public long Length { get => r.BaseStream.Length; }
+
+    /// <summary> Reads data from the given byte array via reader. </summary>
+    public static void Read(byte[] data, Action<Reader> cons)
+    {
+        MemoryStream stream = new MemoryStream(data);
+        using (var r = new BinaryReader(stream)) cons(new Reader(r));
+    }
+
+    /// <summary> Reads a boolean. </summary>
+    public bool Bool() => r.ReadBoolean();
+
+    /// <summary> Reads a byte. </summary>
+    public byte Byte() => r.ReadByte();
+
+    /// <summary> Reads bytes. </summary>
+    public byte[] Bytes(int count) => r.ReadBytes(count);
+
+    /// <summary> Reads a short. </summary>
+    public short Short() => r.ReadInt16();
+
+    /// <summary> Reads an integer. </summary>
+    public int Int() => r.ReadInt32();
+
+    /// <summary> Reads a float. </summary>
+    public float Float() => r.ReadSingle();
+
+    /// <summary> Reads a string. </summary>
+    public string String() => r.ReadString();
+
+    /// <summary> Reads a vector. </summary>
+    public Vector3 Vector() => new Vector3(r.ReadSingle(), r.ReadSingle(), r.ReadSingle());
+
+    /// <summary> Reads a SteamID. </summary>
+    public SteamId Id() => r.ReadUInt64();
+}
---
+++ io/Writer.cs
@@ -0,0 +1,63 @@
+namespace Jaket.IO;
+
+using Steamworks;
+using System;
+using System.IO;
+using UnityEngine;
+
+/// <summary> Builtin binary writer but with some extra methods for convenience. </summary>
+public class Writer
+{
+    /// <summary> Output for writer. </summary>
+    private BinaryWriter w;
+
+    /// <summary> Creates a writer with the given output. </summary>
+    private Writer(BinaryWriter w) => this.w = w;
+
+    /// <summary> Current cursor position in the stream. </summary>
+    public long Position { get => w.BaseStream.Position; }
+
+    /// <summary> Stream length. </summary>
+    public long Length { get => w.BaseStream.Length; }
+
+    /// <summary> Writes data to return byte array via writer. </summary>
+    public static byte[] Write(Action<Writer> cons)
+    {
+        MemoryStream stream = new MemoryStream();
+        using (var w = new BinaryWriter(stream)) cons(new Writer(w));
+
+        return stream.ToArray();
+    }
+
+    /// <summary> Writes a boolean. </summary>
+    public void Bool(bool value) => w.Write(value);
+
+    /// <summary> Writes a byte. </summary>
+    public void Byte(byte value) => w.Write(value);
+
+    /// <summary> Writes bytes. </summary>
+    public void Bytes(byte[] value) => w.Write(value);
+
+    /// <summary> Writes a short. </summary>
+    public void Short(short value) => w.Write(value);
+
+    /// <summary> Writes an integer. </summary>
+    public void Int(int value) => w.Write(value);
+
+    /// <summary> Writes a float. </summary>
+    public void Float(float value) => w.Write(value);
+
+    /// <summary> Writes a string. </summary>
+    public void String(string value) => w.Write(value);
+
+    /// <summary> Writes a vector. </summary>
+    public void Vector(Vector3 value)
+    {
+        w.Write(value.x);
+        w.Write(value.y);
+        w.Write(value.z);
+    }
+
+    /// <summary> Writes a SteamID. </summary>
+    public void Id(SteamId value) => w.Write(value);
+}
