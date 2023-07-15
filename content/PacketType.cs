8415e1f0ad45f3572db4f4f18f1c5caffd951124
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2023-07-15 19:49:29 +0300

    add comments for packet types and a new one type

--- content/PacketType.cs
+++ content/PacketType.cs
@@ -3,8 +3,15 @@ namespace Jaket.Content;
 /// <summary> All packet types. Will replenish over time. </summary>
 public enum PacketType
 {
+    /// <summary> Data of all entities, usually players and enemies. </summary>
     Snapshot,
+    /// <summary> Host asks to load a level, the packet contains its name. </summary>
     LevelLoading,
+    /// <summary> Owner of the lobby has died, which means you need to destroy all enemies. </summary>
+    HostDied,
+
+    /// <summary> Client ask the creation of a bullet. </summary>
     SpawnBullet,
+    /// <summary> Client dealt damage to a player. </summary>
     DamagePlayer
 }
