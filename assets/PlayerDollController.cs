f4f2110042797a8d693d2c2e970d2e2055dc1219
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2023-08-22 23:31:20 +0300

    add 2 emotions for player doll

--- assets/PlayerDollController.cs
+++ assets/PlayerDollController.cs
@@ -1,5 +1,6 @@
 namespace Jaket.Assets;
 
+using System.Collections;
 using UnityEngine;
 
 /// <summary> Class needed for testing animations. </summary>
@@ -15,8 +16,34 @@ public class PlayerDollController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space)) animator.SetTrigger("Jump");
         if (Input.GetKeyDown(KeyCode.F)) animator.SetTrigger("Punch");
 
+        if (Input.GetKeyUp(KeyCode.Alpha1)) StartEmoji(0);
+        if (Input.GetKeyUp(KeyCode.Alpha2)) StartEmoji(1);
+        if (Input.GetKeyUp(KeyCode.Alpha3)) StartEmoji(2);
+        if (Input.GetKeyUp(KeyCode.Alpha4)) StartEmoji(3);
+        if (Input.GetKeyUp(KeyCode.Alpha5)) StartEmoji(4);
+        if (Input.GetKeyUp(KeyCode.Alpha6)) StartEmoji(5);
+
+        if (Input.anyKey) animator.SetInteger("Emoji", -1);
+
         animator.SetBool("Walking", Input.GetKey(KeyCode.W));
         animator.SetBool("Sliding", Input.GetKey(KeyCode.LeftControl));
         animator.SetBool("InAir", Input.GetKey(KeyCode.Space));
+    }
+
+    /// <summary> Triggers an emoji with the given id. </summary>
+    private void StartEmoji(int id)
+    {
+        animator.SetTrigger("Show Emoji");
+        animator.SetInteger("Emoji", id);
+
+        StopCoroutine("ClearEmoji");
+        StartCoroutine("ClearEmoji");
+    }
+
+    /// <summary> Returns the emoji id to -1 after the end of an animation. </summary>
+    private IEnumerator ClearEmoji()
+    {
+        yield return new WaitForSeconds(animator.GetInteger("Emoji") == 0 ? 2.458f : 1.833f);
+        animator.SetInteger("Emoji", -1);
     }
 }
