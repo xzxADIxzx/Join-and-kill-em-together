9dd81e4a3c680049749926429d3fcf11b250f986
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2023-08-23 21:04:11 +0300

    add emoji support to movement

--- world/Movement.cs
+++ world/Movement.cs
@@ -1,5 +1,7 @@
 namespace Jaket.World;
 
+using System.Collections;
+using UMM;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -14,6 +16,13 @@ public class Movement : MonoSingleton<Movement>
     /// <summary> Whether cheats were enabled at the time of blocking movement. </summary>
     private static bool wasCheatsEnabled;
 
+    /// <summary> Emoji selection wheel keybind. </summary>
+    public UKKeyBind EmojiBind;
+    /// <summary> An array containing the length of all emotions in seconds. </summary>
+    public float[] EmojiLegnth = { 2.458f, 0f, 1.833f, 0f, 0f, 0f };
+    /// <summary> Id of the currently playing emoji. </summary>
+    public byte Emoji = 0xFF;
+
     /// <summary> Creates a singleton of movement. </summary>
     public static void Load()
     {
@@ -26,6 +35,16 @@ public class Movement : MonoSingleton<Movement>
 
     public void LateUpdate() // late update is needed in order to overwrite the time scale value
     {
+        // find or create a keybind if it doesn't already exist
+        if (EmojiBind == null) EmojiBind = UKAPI.GetKeyBind("EMOJI WHEEL", KeyCode.G);
+
+        // if the emoji wheel is invisible and the key has been pressed for 0.25 seconds, then show it
+        if (!EmojiWheel.Instance.Shown && EmojiBind.HoldTime > .25f) EmojiWheel.Instance.Show();
+
+        // if the emoji wheel is visible, but the key is not pressed, then hide it
+        if (EmojiWheel.Instance.Shown && !EmojiBind.IsPressedInScene) EmojiWheel.Instance.Hide();
+
+
         // all the following changes are related to the network part of the game and shouldn't affect the local
         if (LobbyController.Lobby == null) return;
 
@@ -35,6 +54,8 @@ public class Movement : MonoSingleton<Movement>
         // sometimes it happens that in the chat the player flies into the air
         if (!NewMovement.Instance.dead) rb.constraints = NewMovement.Instance.enabled ? RigidbodyConstraints.FreezeRotation : RigidbodyConstraints.FreezeAll;
     }
+
+    #region toggling
 
     /// <summary> Toggles the ability to move, used in the chat and etc. </summary>
     public static void ToggleMovement(bool enable)
@@ -66,4 +87,28 @@ public class Movement : MonoSingleton<Movement>
         // block camera rotation
         CameraController.Instance.enabled = !enable;
     }
+
+    #endregion
+    #region emoji
+
+    /// <summary> Triggers an emoji with the given id. </summary>
+    public void StartEmoji(byte id)
+    {
+        Emoji = id;
+
+        StopCoroutine("ClearEmoji");
+        StartCoroutine("ClearEmoji");
+    }
+
+    /// <summary> Returns the emoji id to -1 after the end of an animation. </summary>
+    public IEnumerator ClearEmoji()
+    {
+        // wait for the end of an animation
+        yield return new WaitForSeconds(EmojiLegnth[Emoji]);
+
+        // return the emoji id to -1
+        Emoji = 0xFF;
+    }
+
+    #endregion
 }
