d39569af8b82975a315bb258ceaae38abf559b38
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2023-10-18 18:57:30 +0300

    add rebinding logic for settings

--- ui/Settings.cs
+++ ui/Settings.cs
@@ -10,9 +10,13 @@ public class Settings : CanvasSingleton<Settings>
 {
     /// <summary> List of all key bindings in the mod. </summary>
     public static KeyCode LobbyTab, PlayerList, Settingz, PlayerIndicators, Pointer, Chat, ScrollUp, ScrollDown, EmojiWheel, SelfDestruction;
-
     /// <summary> Gets the key binding value from its path. </summary>
     public static KeyCode GetKey(string path, KeyCode def) => (KeyCode)PrefsManager.Instance.GetInt($"jaket.binds.{path}", (int)def);
+
+    /// <summary> Whether a key binding is being reassigned. </summary>
+    public bool Rebinding;
+    /// <summary> Components of a key remap button and the path to the keybind. </summary>
+    private string path; Text text; Image background;
 
     /// <summary> Loads and applies all settings. </summary>
     public static void Load()
@@ -37,7 +41,7 @@ public class Settings : CanvasSingleton<Settings>
             UI.Text("--CONTROLS--", table, 0f, 316f);
             UI.Button("RESET", table, 0f, 260f);
 
-            int y = 0;
+            int y = 0; // no way
             UI.KeyButton("lobby-tab", LobbyTab, table, 0f, 196f + y-- * 56f);
             UI.KeyButton("player-list", PlayerList, table, 0f, 196f + y-- * 56f);
             UI.KeyButton("settings", Settingz, table, 0f, 196f + y-- * 56f);
@@ -51,6 +55,32 @@ public class Settings : CanvasSingleton<Settings>
         });
     }
 
+    private void OnGUI()
+    {
+        if (!Rebinding) return;
+
+        var current = Event.current; // receive the event and check whether any key is pressed
+        if (!current.isKey && !current.isMouse && !current.shift) return;
+
+        background.color = new(0f, 0f, 0f, .5f);
+        Rebinding = false;
+
+        // cancel key binding remapping
+        if (current.keyCode == KeyCode.Escape) return;
+
+        KeyCode key = current.isKey
+            ? current.keyCode
+            : current.isMouse
+                ? KeyCode.Mouse0 + current.button
+                : Input.GetKeyDown(KeyCode.LeftShift)
+                    ? KeyCode.LeftShift
+                    : KeyCode.RightShift;
+
+        text.text = ControlsOptions.GetKeyName(key);
+        PrefsManager.Instance.SetInt($"jaket.binds.{path}", (int)key);
+        Load(); // update control settings
+    }
+
     // <summary> Toggles visibility of settings. </summary>
     public void Toggle()
     {
@@ -59,5 +89,16 @@ public class Settings : CanvasSingleton<Settings>
 
         gameObject.SetActive(Shown = !Shown);
         Movement.UpdateState();
+    }
+
+    // <summary> Starts rebinding the given key. </summary>
+    public void Rebind(string path, Text text, Image background)
+    {
+        this.path = path;
+        this.text = text;
+        this.background = background;
+
+        background.color = new(1f, .7f, .1f);
+        Rebinding = true;
     }
 }
