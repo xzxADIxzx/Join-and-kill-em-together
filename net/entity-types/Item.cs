38d285e02f2b456e9c941c70fd33e0acf66f26aa
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2023-11-13 21:38:36 +0300

    add altar sync

--- net/entity-types/Item.cs
+++ net/entity-types/Item.cs
@@ -17,11 +17,16 @@ public class Item : Entity
     private FloatLerp x, y, z, rx, ry, rz;
     /// <summary> Reference to the component needed to change the kinematics. </summary>
     private Rigidbody rb;
+    /// <summary> Identifier used to synchronize altars. </summary>
+    private ItemIdentifier itemId;
 
     /// <summary> Player holding an item in their hands. </summary>
     private RemotePlayer player;
     /// <summary> Whether the player is holding an item. </summary>
     private bool holding;
+    /// <summary> Whether the item is placed on an altar. </summary>
+    private bool placed;
+
     /// <summary> Time of last transfer of the item from one client to another. </summary>
     private float lastTransferTime;
 
@@ -40,9 +45,11 @@ public class Item : Entity
 
         // other
         rb = GetComponent<Rigidbody>();
+        itemId = GetComponent<ItemIdentifier>();
+
         if (LobbyController.IsOwner)
         {
-            int index = plushy ? Items.PlushyIndex(transform) : Items.ItemIndex(GetComponent<ItemIdentifier>());
+            int index = plushy ? Items.PlushyIndex(transform) : Items.ItemIndex(itemId);
             if (index == -1)
             {
                 Destroy(this);
@@ -60,6 +67,7 @@ public class Item : Entity
         // update the player holding the item
         if (LastOwner != Owner && Networking.Entities.TryGetValue(LastOwner = Owner, out var entity) && entity is RemotePlayer player) this.player = player;
 
+        // the game itself will update everything for the owner of the item
         if (IsOwner) return;
 
         // turn off object physics so that it does not interfere with synchronization
@@ -69,6 +77,26 @@ public class Item : Entity
             ? this.player.usingHook ? this.player.hook.position : this.player.hookRoot.position
             : new(x.Get(LastUpdate), y.Get(LastUpdate), z.Get(LastUpdate));
         transform.eulerAngles = new(rx.Get(LastUpdate), ry.Get(LastUpdate), rz.Get(LastUpdate));
+
+        // remove from the altar
+        if (!placed && itemId.ipz != null)
+        {
+            itemId.transform.SetParent(null);
+            itemId.ipz.CheckItem();
+            itemId.ipz = null;
+        }
+
+        // put on the altar
+        if (placed && itemId.ipz == null)
+        {
+            var colliders = Physics.OverlapSphere(transform.position, 0.01f, 20971776, QueryTriggerInteraction.Collide);
+            foreach (var col in colliders)
+                if (col.gameObject.layer == 22 && col.TryGetComponent<ItemPlaceZone>(out var zone))
+                {
+                    itemId.transform.SetParent(col.transform);
+                    zone.CheckItem();
+                }
+        }
     }
 
     public void PickUp()
@@ -84,6 +112,7 @@ public class Item : Entity
         w.Vector(transform.position);
         w.Vector(transform.eulerAngles);
         w.Bool(IsOwner ? FistControl.Instance.heldObject?.gameObject == gameObject : holding);
+        w.Bool(IsOwner ? itemId.ipz != null : placed);
     }
 
     public override void Read(Reader r)
@@ -100,6 +129,7 @@ public class Item : Entity
         x.Read(r); y.Read(r); z.Read(r);
         rx.Read(r); ry.Read(r); rz.Read(r);
         holding = r.Bool();
+        placed = r.Bool();
     }
 
     public override void Damage(Reader r) { }
