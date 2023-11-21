0a64c0b9739b92fee4db76e8f476fa7041a90fec
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2023-11-21 19:40:43 +0300

    update version of the mod

--- Version.cs
+++ Version.cs
@@ -7,7 +7,7 @@ using UnityEngine.Networking;
 public class Version
 {
     /// <summary> Current version of the mod installed by the player. </summary>
-    public const string CURRENT = "0.8.18";
+    public const string CURRENT = "1.0.0";
     /// <summary> Repository of the mod, where the newest version will be taken from. </summary>
     public const string REPO = "xzxADIxzx/Join-and-kill-em-together";
     /// <summary> Github API URL. I think it's not difficult to guess. </summary>
