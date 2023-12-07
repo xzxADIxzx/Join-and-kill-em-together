9036e98e1f168492e0a97acd109ae57b4bff178e
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2023-12-07 17:17:02 +0300

    remake writer & reader to work with new API

---
+++ src/Jaket/IO/Reader.cs
@@ -0,0 +1,72 @@
+namespace Jaket.IO;
+
+using Steamworks;
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using UnityEngine;
+
+/// <summary> Wrapper over Marshal for convenience and the ability to read floating point numbers. </summary>
+public class Reader
+{
+    /// <summary> Current cursor position. </summary>
+    public int Position;
+    /// <summary> Allocated memory length. </summary>
+    public readonly int Length;
+
+    /// <summary> Pointer to the allocated memory. </summary>
+    private IntPtr mem;
+    /// <summary> Creates a reader with the given memory. </summary>
+    private Reader(IntPtr memory, int length) { this.mem = memory; this.Length = length; }
+
+    /// <summary> Reads data from the given memory via reader. </summary>
+    public static void Read(IntPtr memory, int length, Action<Reader> cons) => cons(new(memory, length));
+
+    /// <summary> Converts integer to float. </summary>
+    public static unsafe float Int2Float(int value) => *(float*)(&value);
+    /// <summary> Converts long to ulong. </summary>
+    public static unsafe ulong Long2Ulong(long value) => *(ulong*)(&value);
+
+    /// <summary> Moves the cursor by a given number of bytes and returns the old cursor position. </summary>
+    public int Inc(int amount)
+    {
+        if (Position < 0) throw new IndexOutOfRangeException("Attempt to read data at a negative index.");
+        Position += amount;
+
+        if (Position > Length) throw new IndexOutOfRangeException("Attempt to read more bytes than were allocated in memory.");
+        return Position - amount;
+    }
+
+    #region types
+
+    public bool Bool() => Marshal.ReadByte(mem, Inc(1)) == 0xFF;
+
+    public byte Byte() => Marshal.ReadByte(mem, Inc(1));
+
+    public byte[] Bytes(int amount)
+    {
+        var bytes = new byte[amount];
+        Marshal.Copy(mem, bytes, Inc(amount), amount);
+        return bytes;
+    }
+
+    public byte[] AllBytes()
+    {
+        Position = 0;
+        return Bytes((int)Length);
+    }
+
+    public int Int() => Marshal.ReadInt32(mem, Inc(4));
+
+    public float Float() => Int2Float(Marshal.ReadInt32(mem, Inc(4)));
+
+    public string String() => Encoding.Unicode.GetString(Bytes(Int()));
+
+    public Vector3 Vector() => new(Float(), Float(), Float());
+
+    public Color32 Color() => new(Byte(), Byte(), Byte(), Byte());
+
+    public SteamId Id() => Long2Ulong(Marshal.ReadInt64(mem, Inc(8)));
+
+    #endregion
+}
---
+++ src/Jaket/IO/Writer.cs
@@ -0,0 +1,77 @@
+namespace Jaket.IO;
+
+using Steamworks;
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using UnityEngine;
+
+/// <summary> Wrapper over Marshal for convenience and the ability to write floating point numbers. </summary>
+public class Writer
+{
+    /// <summary> Current cursor position. </summary>
+    public int Position;
+    /// <summary> Allocated memory length. </summary>
+    public readonly int Length;
+
+    /// <summary> Pointer to the allocated memory. </summary>
+    private IntPtr mem;
+    /// <summary> Creates a writer with the given memory. </summary>
+    private Writer(IntPtr memory, int length) { this.mem = memory; this.Length = length; }
+
+    /// <summary> Allocates memory and writes data there. </summary>
+    public static void Write(Action<Writer> cons, Action<IntPtr, int> result, int memoryAmount = 96)
+    {
+        Writer instance = new(Marshal.AllocHGlobal(memoryAmount), memoryAmount);
+        cons(instance);
+        result(instance.mem, instance.Position); // 96 bytes are allocated in memory by default, which is enough for each entity, but not all of this memory is used
+        Marshal.FreeHGlobal(instance.mem);
+    }
+
+    /// <summary> Converts float to integer. </summary>
+    public static unsafe int Float2Int(float value) => *(int*)(&value);
+    /// <summary> Converts ulong to long. </summary>
+    public static unsafe long ULong2long(ulong value) => *(long*)(&value);
+
+    /// <summary> Moves the cursor by a given number of bytes and returns the old cursor position. </summary>
+    public int Inc(int amount)
+    {
+        if (Position < 0) throw new IndexOutOfRangeException("Attempt to write data at a negative index.");
+        Position += amount;
+
+        if (Position > Length) throw new IndexOutOfRangeException("Attempt to write more bytes than were allocated in memory.");
+        return Position - amount;
+    }
+
+    #region types
+
+    public void Bool(bool value) => Marshal.WriteByte(mem, Inc(1), (byte)(value ? 0xFF : 0x00));
+
+    public void Byte(byte value) => Marshal.WriteByte(mem, Inc(1), value);
+
+    public void Bytes(byte[] value) => Marshal.Copy(value, 0, mem + Inc(value.Length), value.Length);
+
+    public void Int(int value) => Marshal.WriteInt32(mem, Inc(4), value);
+
+    public void Float(float value) => Marshal.WriteInt32(mem, Inc(4), Float2Int(value));
+
+    public void String(string value)
+    {
+        value ??= "";
+        Int(value.Length);
+        Bytes(Encoding.Unicode.GetBytes(value));
+    }
+
+    public void Vector(Vector3 value)
+    {
+        Float(value.x);
+        Float(value.y);
+        Float(value.z);
+    }
+
+    public void Color(Color32 value) => Int(value.rgba);
+
+    public void Id(SteamId value) => Marshal.WriteInt64(mem, Inc(8), ULong2long(value));
+
+    #endregion
+}
