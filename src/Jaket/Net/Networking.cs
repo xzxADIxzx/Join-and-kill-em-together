976f06af8e7b5558c9ffb1226bfbc58c56c982cc
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2023-12-08 16:29:05 +0300

    update networking

--- src/Jaket/Net/Networking.cs
+++ src/Jaket/Net/Networking.cs
@@ -1,21 +1,24 @@
 namespace Jaket.Net;
 
+using HarmonyLib;
+using Steamworks;
+using Steamworks.Data;
 using System;
 using System.Collections.Generic;
-using Steamworks;
 using UnityEngine;
 
 using Jaket.Content;
 using Jaket.IO;
-using Jaket.Net.EndPoints;
-using Jaket.Net.EntityTypes;
+using Jaket.Net.Endpoints;
+using Jaket.Net.Types;
 using Jaket.UI;
 using Jaket.World;
 
+/// <summary> Class responsible for updating endpoints, transmitting packets and managing entities. </summary>
 public class Networking : MonoSingleton<Networking>
 {
     /// <summary> Number of snapshots to be sent per second. </summary>
-    public const int SNAPSHOTS_PER_SECOND = 18;
+    public const int SNAPSHOTS_PER_SECOND = 16;
     /// <summary> Number of seconds between snapshots. </summary>
     public const float SNAPSHOTS_SPACING = 1f / SNAPSHOTS_PER_SECOND;
 
@@ -24,13 +27,11 @@ public class Networking : MonoSingleton<Networking>
     /// <summary> Client endpoint. Will be updated by players connected to the lobby. </summary>
     public static Client Client = new();
 
-    /// <summary> List of all entities synchronized between clients. May contain null if the entity was destroyed via Object.Destroy. </summary>
+    /// <summary> List of all entities by their id. May contain null. </summary>
     public static Dictionary<ulong, Entity> Entities = new();
-    /// <summary> List of all bosses on the map. Needed for the internal logic of the game. </summary>
-    public static List<EnemyIdentifier> Bosses = new();
-
-    /// <summary> Local player representation. </summary>
+    /// <summary> Local player singleton. </summary>
     public static LocalPlayer LocalPlayer;
+
     /// <summary> Whether a scene is loading right now. </summary>
     public static bool Loading;
     /// <summary> Whether multiplayer was used in the current level. </summary>
@@ -42,6 +43,11 @@ public class Networking : MonoSingleton<Networking>
         Server.Load();
         Client.Load();
 
+        // create an object to update the network logic
+        UI.Object("Networking").AddComponent<Networking>();
+        // create a local player to sync player data
+        LocalPlayer = UI.Object("Local Player", Instance.transform).AddComponent<LocalPlayer>();
+
         Events.OnLoaded += () => WasMultiplayerUsed = LobbyController.Lobby.HasValue;
         Events.OnLobbyAction += () => WasMultiplayerUsed |= LobbyController.Lobby.HasValue;
 
@@ -50,28 +56,19 @@ public class Networking : MonoSingleton<Networking>
             Clear(); // for safety
             Loading = false;
 
-            if (LobbyController.IsOwner)
-            {
-                // re-add a local player, because the list was cleared 
-                Entities[LocalPlayer.Id] = LocalPlayer;
+            // re-add the local player, because the list was cleared 
+            Entities[LocalPlayer.Id] = LocalPlayer;
 
-                // inform all players about the transition to a new level
-                Redirect(World.Instance.WriteData(), PacketType.LevelLoading);
-            }
+            // inform all players about the transition to a new level
+            if (LobbyController.IsOwner) Writer.Write(w =>
+            {
+                w.Enum(PacketType.LevelLoading);
+                World.Instance.WriteData(w);
+            }, Redirect);
         };
 
         // fires when accepting an invitation via the Steam overlay
         SteamFriends.OnGameLobbyJoinRequested += (lobby, id) => LobbyController.JoinLobby(lobby);
-
-        SteamMatchmaking.OnChatMessage += (lobby, member, message) =>
-        {
-            if (message.StartsWith("<system>")) // I think it's okay
-                Chat.Instance.ReceiveChatMessage(message.Substring("<system>".Length));
-            else if (message.StartsWith("/tts "))
-                Chat.Instance.ReceiveTTSMessage(member, message.Substring("/tts ".Length));
-            else
-                Chat.Instance.ReceiveChatMessage(GetTeamColor(member), member.Name, message);
-        };
 
         SteamMatchmaking.OnLobbyEntered += lobby =>
         {
@@ -81,17 +78,21 @@ public class Networking : MonoSingleton<Networking>
             PlayerIndicators.Instance.gameObject.SetActive(PlayerIndicators.Shown = true);
             if (!PlayerInfo.Shown) PlayerInfo.Instance.Toggle();
 
-            // destroy all entities, since the player could join from another lobby, and sync all items
-            Clear();
+            Clear(); // destroy all entities, since the player could join from another lobby, and sync all items
             Items.SyncAll();
 
             if (LobbyController.IsOwner)
+            {
+                // open the server so people can join it
+                Server.Open();
+
                 // the lobby has just been created, so just add the local player to the list of entities
                 Entities[LocalPlayer.Id] = LocalPlayer;
+            }
             else
             {
                 // establishing a connection with the owner of the lobby
-                SteamNetworking.AcceptP2PSessionWithUser(lobby.Owner.Id);
+                Client.Connect(lobby.Owner.Id);
 
                 // prevent objects from loading before the scene is loaded
                 Loading = true;
@@ -106,11 +107,12 @@ public class Networking : MonoSingleton<Networking>
             // send notification to chat
             lobby.SendChatString($"<system><color=#00FF00>Player {member.Name} joined!</color>");
 
-            // confirm the connection with the player
-            SteamNetworking.AcceptP2PSessionWithUser(member.Id);
-
             // send the current scene name to the player
-            Send(member.Id, World.Instance.WriteData(), PacketType.LevelLoading);
+            Writer.Write(w =>
+            {
+                w.Enum(PacketType.LevelLoading);
+                World.Instance.WriteData(w);
+            }, (data, size) => FindCon(member.Id)?.SendMessage(data, size));
         };
 
         SteamMatchmaking.OnLobbyMemberLeave += (lobby, member) =>
@@ -129,48 +131,34 @@ public class Networking : MonoSingleton<Networking>
                 Entities[member.Id] = null;
             }
 
-            // returning the exited player's items back to the host owner
-            if (LobbyController.IsOwner) EachItem(item =>
+            // returning the exited player's items back to the host owner & close the connection
+            if (LobbyController.IsOwner)
             {
-                if (item.Owner == member.Id) item.Owner = SteamClient.SteamId;
-            });
+                FindCon(member.Id)?.Close();
+                EachItem(item =>
+                {
+                    if (item.Owner == member.Id) item.Owner = SteamClient.SteamId;
+                });
+            }
         };
-
-        // create a local player to sync player data
-        LocalPlayer = UI.Object("Local Player").AddComponent<LocalPlayer>();
-
-        // create an object to update the network logic
-        UI.Object("Networking").AddComponent<Networking>();
     }
 
-    /// <summary> Destroys all network objects and clears lists. </summary>
+    /// <summary> Destroys all players and clears lists. </summary>
     public static void Clear()
     {
-        // we destroy all players, but leave the enemies
         EachPlayer(player => Destroy(player.gameObject));
-
         Entities.Clear();
-        Bosses.Clear();
     }
+
+    #region cycle
 
     private void Start() => InvokeRepeating("NetworkUpdate", 0f, SNAPSHOTS_SPACING);
 
-    /// <summary> Core network logic. Part of it is moved to the server and the client. </summary>
-    public void NetworkUpdate()
+    /// <summary> Core network logic should have been here, but in fact it is located in the server and client classes. </summary>
+    private void NetworkUpdate()
     {
         // the player isn't connected to the lobby, and no logic needs to be updated
         if (LobbyController.Lobby == null) return;
-
-        // first level loading when connected to a lobby
-        if (Loading)
-        {
-            if (SteamNetworking.IsP2PPacketAvailable((int)PacketType.LevelLoading))
-            {
-                var packet = SteamNetworking.ReadP2PPacket((int)PacketType.LevelLoading);
-                if (packet.HasValue) Reader.Read(packet.Value.Data, World.Instance.ReadData);
-            }
-            return; // during loading, look only for packages with the name of the desired scene
-        }
 
         // update the server or client depending on the role of the player
         if (LobbyController.IsOwner)
@@ -179,7 +167,14 @@ public class Networking : MonoSingleton<Networking>
             Client.Update();
     }
 
+    #endregion
     #region iteration
+
+    /// <summary> Iterates each connection. </summary>
+    public static void EachConnection(Action<Connection> cons)
+    {
+        foreach (var con in Server.Manager.Connected) cons(con);
+    }
 
     /// <summary> Iterates each entity. </summary>
     public static void EachEntity(Action<Entity> cons)
@@ -203,39 +198,35 @@ public class Networking : MonoSingleton<Networking>
     }
 
     #endregion
-    #region communication
-
-    /// <summary> Sends packet data to the receiver over a reliable channel. </summary>
-    public static void Send(SteamId receiver, byte[] data, PacketType packetType, P2PSend sendType = P2PSend.Reliable)
-            => SteamNetworking.SendP2PPacket(receiver, data, nChannel: (int)packetType, sendType: sendType);
-
-    /// <summary> Sends packet data to the receiver over an unreliable channel. </summary>
-    public static void SendSnapshot(SteamId receiver, byte[] data)
-            => Send(receiver, data, PacketType.Snapshot, P2PSend.Unreliable);
-
-    /// <summary> Sends an empty packet to the receiver over a reliable channel. </summary>
-    public static void SendEmpty(SteamId receiver, PacketType packetType)
-            => Send(receiver, new byte[1], packetType);
-
-    /// <summary> Forwards packet data to all clients or to a host that will forward to all clients. </summary>
-    public static void Redirect(byte[] data, PacketType packetType)
-    {
-        if (LobbyController.IsOwner)
-            LobbyController.EachMemberExceptOwner(member => Send(member.Id, data, packetType));
-        else
-            Send(LobbyController.Owner, data, packetType);
-    }
-
-    #endregion
-    #region teams
+    #region tools
 
     /// <summary> Returns the team of the given friend. </summary>
     public static Team GetTeam(Friend friend) => friend.IsMe
         ? LocalPlayer.Team
-        : (Entities.TryGetValue(friend.Id, out var entity) && entity is RemotePlayer player ? player.team : Team.Yellow);
+        : (Entities.TryGetValue(friend.Id, out var entity) && entity != null && entity is RemotePlayer player ? player.team : Team.Yellow);
 
     /// <summary> Returns the hex color of the friend's team. </summary>
     public static string GetTeamColor(Friend friend) => ColorUtility.ToHtmlStringRGBA(GetTeam(friend).Data().Color());
+
+    /// <summary> This class is a little broken, so you have to use crutches. </summary>
+    public static NetIdentity GetIdentity(ConnectionInfo info) => (NetIdentity)AccessTools.DeclaredField(typeof(ConnectionInfo), 0).GetValue(info);
+
+    /// <summary> Finds a connection by id or returns null if there is no such connection. </summary>
+    public static Connection? FindCon(SteamId id)
+    {
+        foreach (var con in Server.Manager.Connected)
+            if (con.ConnectionName == id.ToString()) return con;
+        return null;
+    }
+
+    /// <summary> Forwards the packet to all clients or the host. </summary>
+    public static void Redirect(IntPtr data, int size)
+    {
+        if (LobbyController.IsOwner)
+            EachConnection(con => con.SendMessage(data, size));
+        else
+            Client.Manager.Connection.SendMessage(data, size);
+    }
 
     #endregion
 }
