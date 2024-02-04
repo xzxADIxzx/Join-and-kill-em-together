bfd8d8c803cd93e6c01c1d00aa6349c091382f25
Ardub <ardub92@gmail.com>
2024-02-05 00:12:18 +0300

    UI Fixes

--- src/Jaket/UI/SpraySettings.cs
+++ src/Jaket/UI/SpraySettings.cs
@@ -34,24 +34,24 @@ public class SpraySettings : CanvasSingleton<SpraySettings>
         UpdateSettings();
 
         UI.Shadow("Shadow", transform);
-        UI.TableAT("Sprays", transform, 0f, 352f, 730f, table =>
+        UI.TableAT("Sprays", transform, 0f, 352f, 826f, table =>
         {
-            UI.Text("--SPRAYS--", table, 0f, 333f);
+            UI.Text("--SPRAYS--", table, 0f, 381f);
 
             SprayTable = table;
 
-            UI.Button("Show folder", table, 0f, -317f, clicked: () => Application.OpenURL($"file://{SprayManager.SpraysPath.Replace("\\", "/")}"));
-            UI.Button("Update", table, 0f, -261f, clicked: () =>
+            UI.Button("Show folder", table, 0f, -373f, clicked: () => Application.OpenURL($"file://{SprayManager.SpraysPath.Replace("\\", "/")}"));
+            UI.Button("Update", table, 0f, -317f, clicked: () =>
             {
                 SprayManager.LoadFileSprays();
                 Rebuild();
             });
-            SprayImage = UI.ImageFromTexture("Spray preview", table, 0f, -140f, Texture2D.whiteTexture, 144f, 144f);
+            SprayImage = UI.ImageFromTexture("Spray preview", table, 0f, -150f, Texture2D.whiteTexture, 256f, 256f);
         });
-        UI.TableAT("Settings", transform, 746f, 352f, 224f, table =>
+        UI.TableAT("Settings", transform, 842f, 352f, 200f, table =>
         {
-            UI.Text("--SETTINGS--", table, 0f, 80f);
-            UI.Toggle("DISABLE SPRAYS", table, 0f, 40f, size: 16, clicked: force =>
+            UI.Text("--SETTINGS--", table, 0f, 68f);
+            UI.Toggle("DISABLE SPRAYS", table, 0f, 20f, size: 16, clicked: force =>
             {
                 prefs.SetBool("jaket.disable-sprays", DisableSprays = force);
             }).isOn = DisableSprays;
@@ -78,32 +78,52 @@ public class SpraySettings : CanvasSingleton<SpraySettings>
         gameObject.SetActive(Shown = toggle);
     }
 
-    public string GetSprayInfoString(string spray) => $"<size=20><color=white>Current spray is:\n{spray}</color></size>\nYour spray applies when scene is changed or player is joined\n<size=16>Max size is {SprayFile.ImageMaxSize / 1024f:0.##}kb</size>";
+    public string GetSprayCurrentString(string spray) => $"<size=20><color=white>Current spray is:\n{spray}</color></size>";
+    public string GetSprayInfoString(string spray) => $"{GetSprayCurrentString(spray)}\nYour spray applies when scene is changed or player is joined\n<size=16>Max size is {SprayFile.ImageMaxSize / 1024f:0.##}kb</size>";
 
     public void Rebuild()
     {
-        SprayManager.ChangeFileSpray(SelectedSpray);
+        if (!SprayManager.ChangeFileSpray(SelectedSpray)) SelectedSpray = "";
 
         // destroy all buttons and clear the list
         SprayButtons.ForEach(b => Destroy(b.gameObject));
         SprayButtons.Clear();
         if (SprayInfoText != null) Destroy(SprayInfoText.gameObject);
 
-        for (int i = 0; i < Mathf.Min(SprayManager.FileSprays.Count, 5); i++) // limit to 5 sprays shown at once
+        for (int i = 0; i < 5; i++) // limit to 5 sprays shown at once
         {
-            var spray = SprayManager.FileSprays[i];
-            SprayButtons.Add(UI.IconTextureButton(spray.GetShortName(), spray.Texture, SprayTable, 0f, 277f - i * 54f, clicked: () => {
-                if (spray.CheckSize()) // check if the spray is too big 
+            if (i < SprayManager.FileSprays.Count)
+            {
+                var spray = SprayManager.FileSprays[i];
+                SprayButtons.Add(UI.IconTextureButton(spray.GetShortName(), spray.Texture, SprayTable, 0f, 325f - i * 54f, clicked: () =>
                 {
-                    UI.SendMsg($"<color=red>Spray is too large. Please, choose another one.</color>\nMax size is {SprayFile.ImageMaxSize / 1024f:0.##}kb."); 
-                    return; 
-                }
-                ChangeSpray(spray.Name);
-            }, color: spray.CheckSize() ? Color.red : Color.white)); // set color based if spray is too big
-            if (i == SprayManager.FileSprays.Count - 1)
-                SprayInfoText = UI.Text(GetSprayInfoString(SprayManager.CurrentSpray != null ? SprayManager.CurrentSpray.GetShortName(18) : "<color=gray>None</color>"), // show current spray info
-                    SprayTable, 0f, 277f - i * 54f - 72f, height: 72f, color: Color.gray, size: 17);
+                    if (spray.CheckSize()) // check if the spray is too big 
+                    {
+                        UI.SendMsg($"<color=red>Spray is too large. Please, choose another one.</color>\nMax size is {SprayFile.ImageMaxSize / 1024f:0.##}kb.");
+                        return;
+                    }
+                    ChangeSpray(spray.Name);
+                }, color: spray.CheckSize() ? Color.red : Color.white)); // set color based if spray is too big
+            }
+            else
+            {
+                var t = new Texture2D(1, 1);
+                t.SetPixel(0, 0, Color.gray);
+                t.Apply();
+
+                var btn = UI.IconTextureButton("", t, SprayTable, 0f, 325f - i * 54f, color: Color.gray);
+                btn.interactable = false;
+                SprayButtons.Add(btn);
+            }
         };
+
+        static string GetNameString() => SprayManager.CurrentSpray != null ? SprayManager.CurrentSpray.GetShortName(18) : "<color=gray>None</color>";
+
+        if (SprayManager.FileSprays.Count == 0)
+            SprayInfoText = UI.Text($"No sprays found. Add one to the folder.\n{GetSprayCurrentString(GetNameString())}", SprayTable, 0f, 40f, height: 72f, color: Color.gray, size: 17);
+        else
+            SprayInfoText = UI.Text(GetSprayInfoString(GetNameString()), // show current spray info
+                    SprayTable, 0f, 32f, height: 88f, color: Color.gray, size: 17);
 
         if (SprayManager.CurrentSpray != null)
         {
