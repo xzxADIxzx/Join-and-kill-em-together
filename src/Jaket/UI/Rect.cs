e2651db31230f11bfffa51c41c4cd4fcacf985d6
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2024-03-10 16:45:14 +0300

    update rect & canvas singleton

--- src/Jaket/UI/Rect.cs
+++ src/Jaket/UI/Rect.cs
@@ -15,15 +15,23 @@ public struct Rect
     public Rect(float x, float y, float width, float height, Vector2 min, Vector2 max)
     { this.x = x; this.y = y; Width = width; Height = height; Min = min; Max = max; }
 
+    public Rect(float x, float y, float width, float height) : this(x, y, width, height, new(.5f, .5f), new(.5f, .5f)) { }
+
     /// <summary> Creates a rect with the default width and anchor of the table. </summary>
-    public static Rect Tlw(float y, float height) => new(168f, y, 336f, height, new(0f, 1f), new(0f, 1f));
+    public static Rect Tlw(float y, float height) => new(16f + 168f, -y, 336f, height, new(0f, 1f), new(0f, 1f));
 
     /// <summary> Creates a rect with the default size of the button. </summary>
-    public static Rect Btn(float x, float y) => new(x, y, 320f, 40f, new(.5f, 1f), new(.5f, 1f));
+    public static Rect Btn(float x, float y) => new(x, -y, 320f, 40f, new(.5f, 1f), new(.5f, 1f));
+
+    /// <summary> Creates a rect with the default size of the toggle. </summary>
+    public static Rect Tgl(float x, float y) => new(x, -y, 320f, 32f, new(.5f, 1f), new(.5f, 1f));
 
     /// <summary> Creates a rect with the default size of the icon button. </summary>
-    public static Rect Icon(float x, float y) => new(x, y, 40f, 40f, new(.5f, 1f), new(.5f, 1f));
+    public static Rect Icon(float x, float y) => new(x, -y, 40f, 40f, new(.5f, 1f), new(.5f, 1f));
 
     /// <summary> Creates a rect at the center of the canvas with the given size. </summary>
-    public static Rect Size(float width, float height) => new(0f, 0f, width, height, new(.5f, .5f), new(.5f, .5f));
+    public static Rect Size(float width, float height) => new(0f, 0f, width, height);
+
+    /// <summary> Creates a new rect at the center of the current with the same size. </summary>
+    public readonly Rect ToText() => new(0f, 1f, Width, Height);
 }
