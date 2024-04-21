016c90595e2f5ba2228af70491dec20b08f628c1
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2024-04-21 17:10:19 +0300

    update nextid algorithm to make it work for clients

--- src/Jaket/Net/Entities.cs
+++ src/Jaket/Net/Entities.cs
@@ -12,7 +12,7 @@ public class Entities
     /// <summary> Dictionary of entity types to their providers. </summary>
     public static Dictionary<EntityType, Prov> Providers = new();
     /// <summary> Last used id, next id's are guaranteed to be greater than it. </summary>
-    public static ulong LastId;
+    public static uint LastId;
 
     /// <summary> Loads providers into the dictionary. </summary>
     public static void Load()
@@ -48,7 +48,7 @@ public class Entities
     }
 
     /// <summary> Returns an entity of the given type. </summary>
-    public static Entity Get(ulong id, EntityType type)
+    public static Entity Get(uint id, EntityType type)
     {
         var entity = Providers[type]();
         if (entity == null) return null;
@@ -62,20 +62,13 @@ public class Entities
     /// <summary> Entity provider. </summary>
     public delegate Entity Prov();
 
-    /// <summary> Returns whether the last id has a collision with any player's id. </summary>
-    public static bool HasCollisionWithPlayerId()
+    /// <summary> Returns the next available id, skips ids of all existing entities. </summary>
+    public static uint NextId()
     {
-        foreach (var member in LobbyController.Lobby?.Members)
-            if (member.Id == LastId) return true;
+        if (LastId < Tools.AccId) LastId = Tools.AccId;
 
-        return false;
-    }
-
-    /// <summary> Returns the next available id, skips the id of all players. </summary>
-    public static ulong NextId()
-    {
-        do LastId++;
-        while (HasCollisionWithPlayerId());
+        LastId++;
+        while (Networking.Entities.ContainsKey(LastId)) LastId += 8192;
 
         return LastId;
     }
