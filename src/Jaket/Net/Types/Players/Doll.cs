ce40246357244a791dc7dcb21fccaf7869ec97a3
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2024-04-26 16:15:05 +0300

    add transforms getters

--- src/Jaket/Net/Types/Players/Doll.cs
+++ src/Jaket/Net/Types/Players/Doll.cs
@@ -21,8 +21,28 @@ public class Doll : MonoBehaviour
     /// <summary> Event triggered after the start of emoji. </summary>
     public Action OnEmojiStart = () => { };
 
+    /// <summary> Transforms of different parts of the body. </summary>
+    public Transform Head, Hand, Hook, HookRoot, Throne, Coin, Skateboard, Suits;
     /// <summary> Slide and fall particles transforms. </summary>
     public Transform SlideParticle, FallParticle;
+
+    /// <summary> Position in which the doll holds an item. </summary>
+    public Vector3 HoldPosition => Hooking ? Hook.position : HookRoot.position;
+
+    private void Awake()
+    {
+        Transform V3 = transform.Find("V3"), rig = transform.Find("Metarig");
+
+        Head = rig.Find("Spine 0/Spine 1/Spine 2");
+        Hand = rig.Find("Spine 0/Right Shoulder/Right Elbow/Right Wrist");
+        Hand = Tools.Create("Weapons Root", Hand).transform;
+        Hook = rig.Find("Hook");
+        HookRoot = rig.Find("Spine 0/Left Shoulder/Left Elbow/Left Wrist/Left Palm");
+        Throne = rig.Find("Throne");
+        Coin = V3.Find("Coin");
+        Skateboard = V3.Find("Skateboard");
+        Suits = V3.Find("Suits");
+    }
 
     private void Update()
     {
@@ -40,7 +60,7 @@ public class Doll : MonoBehaviour
         if (WasHooking != Hooking)
         {
             if (WasHooking = Hooking) Animator.SetTrigger("hook");
-            // hook.gameObject.SetActive(Hooking);
+            Hook.gameObject.SetActive(Hooking);
         }
         if (WasShopping != Shopping && (WasShopping = Shopping)) Animator.SetTrigger("open-shop");
 
@@ -49,6 +69,10 @@ public class Doll : MonoBehaviour
             Animator.SetTrigger("show-emoji");
             Animator.SetInteger("emoji", LastEmoji = Emoji);
             Animator.SetInteger("rps", Rps);
+
+            Throne.gameObject.SetActive(Emoji == 6);
+            Coin.gameObject.SetActive(Emoji == 7);
+            Skateboard.gameObject.SetActive(Emoji == 11);
 
             OnEmojiStart();
         }
