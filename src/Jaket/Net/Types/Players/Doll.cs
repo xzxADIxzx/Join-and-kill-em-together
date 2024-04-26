82eda6bb1916b33094ec7190f8af6bab04fcad38
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2024-04-26 15:47:40 +0300

    move particles and emoji to Doll.cs

--- src/Jaket/Net/Types/Players/Doll.cs
+++ src/Jaket/Net/Types/Players/Doll.cs
@@ -1,5 +1,6 @@
 namespace Jaket.Net.Types.Players;
 
+using System;
 using UnityEngine;
 
 using Jaket.IO;
@@ -8,12 +9,20 @@ using Jaket.IO;
 /// Doll of a player, remote from network or local from emoji.
 /// Responsible for the visual part of the player, i.e. suits and animations.
 /// </summary>
-public class Doll
+public class Doll : MonoBehaviour
 {
     /// <summary> Component rendering animations of the doll. </summary>
     public Animator Animator;
     /// <summary> Animator states that affect which animation will be played. </summary>
     public bool Walking, Sliding, Falling, InAir, WasInAir, Dashing, WasDashing, Riding, WasRiding, Hooking, WasHooking, Shopping, WasShopping;
+
+    /// <summary> Emoji that plays at the moment. </summary>
+    public byte Emoji, LastEmoji = 0xFF, Rps;
+    /// <summary> Event triggered after the start of emoji. </summary>
+    public Action OnEmojiStart = () => { };
+
+    /// <summary> Slide and fall particles transforms. </summary>
+    public Transform SlideParticle, FallParticle;
 
     private void Update()
     {
@@ -34,6 +43,33 @@ public class Doll
             // hook.gameObject.SetActive(Hooking);
         }
         if (WasShopping != Shopping && (WasShopping = Shopping)) Animator.SetTrigger("open-shop");
+
+        if (LastEmoji != Emoji)
+        {
+            Animator.SetTrigger("show-emoji");
+            Animator.SetInteger("emoji", LastEmoji = Emoji);
+            Animator.SetInteger("rps", Rps);
+
+            OnEmojiStart();
+        }
+
+        if (Sliding && SlideParticle == null)
+        {
+            SlideParticle = Instantiate(NewMovement.Instance.slideParticle, transform).transform;
+            SlideParticle.localPosition = new(0f, 0f, 3.5f);
+            SlideParticle.localEulerAngles = new(0f, 180f, 0f);
+            SlideParticle.localScale = new(1.5f, 1f, .8f);
+        }
+        else if (!Sliding && SlideParticle != null) Destroy(SlideParticle.gameObject);
+
+        if (Falling && FallParticle == null)
+        {
+            FallParticle = Instantiate(NewMovement.Instance.fallParticle, transform).transform;
+            FallParticle.localPosition = new(0f, 6f, 0f);
+            FallParticle.localEulerAngles = new(90f, 0f, 0f);
+            FallParticle.localScale = new(1.2f, .6f, 1f);
+        }
+        else if (!Falling && FallParticle != null) Destroy(FallParticle.gameObject);
     }
 
     #region entity
