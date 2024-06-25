784ab1b872163f1ea07ba4a7e418fa7464c754f0
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2024-06-25 12:42:22 +0300

    rewrite security system & brain

--- src/Jaket/Net/Types/Enemies/Brain.cs
+++ src/Jaket/Net/Types/Enemies/Brain.cs
@@ -6,11 +6,9 @@ using Jaket.Content;
 using Jaket.IO;
 using Jaket.World;
 
-/// <summary> Brain representation responsible for synchronizing health and idols. </summary>
-public class Brain : Entity
+/// <summary> Representation of Earthmover's brain. </summary>
+public class Brain : Enemy
 {
-    /// <summary> Earthmover's brain health. </summary>
-    private FloatLerp health = new();
     /// <summary> Idols' state. </summary>
     private bool idol1, idol2;
     /// <summary> Idols' doors. </summary>
@@ -20,30 +18,32 @@ public class Brain : Entity
     {
         Init(_ => EntityType.Brain);
 
-        transform.parent.Find("IdolPod").GetChild(0).TryGetComponent(out door1);
-        transform.parent.Find("IdolPod (1)").GetChild(0).TryGetComponent(out door2);
-
-        if (LobbyController.IsOwner)
-            LobbyController.ScaleHealth(ref EnemyId.machine.health);
-
+        Owner = LobbyController.LastOwner.AccountId;
         World.Brain = this;
     }
 
-    private void Update()
+    private void Start()
     {
-        if (LobbyController.IsOwner) return;
+        Boss(true, 100f, 1);
 
-        EnemyId.machine.health = health.Get(LastUpdate);
+        transform.parent.Find("IdolPod/Cylinder").TryGetComponent(out door1);
+        transform.parent.Find("IdolPod (1)/Cylinder").TryGetComponent(out door2);
+    }
+
+    private void Update() => Stats.MTE(() =>
+    {
+        if (IsOwner || Dead) return;
 
         if (door1.open && !idol1) door1.Close();
         if (door2.open && !idol2) door2.Close();
-    }
+    });
 
     #region entity
 
     public override void Write(Writer w)
     {
-        w.Float(EnemyId.health);
+        UpdatesCount++;
+
         w.Bool(door1.open);
         w.Bool(door2.open);
     }
@@ -52,12 +52,9 @@ public class Brain : Entity
     {
         LastUpdate = Time.time;
 
-        health.Read(r);
         idol1 = r.Bool();
         idol2 = r.Bool();
     }
-
-    public override void Kill(Reader r) => EnemyId.InstaKill();
 
     #endregion
 }
---
+++ src/Jaket/Net/Types/Enemies/SecuritySystem.cs
@@ -0,0 +1,9 @@
+namespace Jaket.Net.Types;
+
+/// <summary> Representation of a part of the security system. </summary>
+public class SecuritySystem : Enemy
+{
+    private void Awake() => Init(_ => Type);
+
+    private void Start() => Boss(true, 15f, 0);
+}
