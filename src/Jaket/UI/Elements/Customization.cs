00664678ff7835d28e86543290ccb918c6c30339
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2024-08-22 12:43:51 +0300

    add preview to the terminal custom element

--- src/Jaket/UI/Elements/Customization.cs
+++ src/Jaket/UI/Elements/Customization.cs
@@ -3,6 +3,7 @@ namespace Jaket.UI.Elements;
 using UnityEngine;
 using UnityEngine.UI;
 
+using Jaket.Assets;
 using Jaket.IO;
 using Jaket.Net;
 
@@ -33,7 +34,7 @@ public class Customization : MonoBehaviour
             UIB.Image("Border", canvas, Fill, shopc, fill: false); // 15 units offset
 
             UIB.Text("#custom.support", canvas, new(0f, -32f, 4200f, 4200f, new(.5f, 1f)), size: 320).transform.localScale /= 10f;
-            UIB.BMaCButton("Buy Me a Coffee", canvas).transform.localPosition = new(0f, 190f, -15f);
+            UIB.BMaCButton("Buy Me a Coffee", canvas).transform.localPosition = new(0f, 190f, 0f);
 
             UIB.Table("Button", canvas, new(-111f, 48f, 206f, 64f, new(.5f, 0f)), button => UIB.ShopButton("#custom.hats", button, Fill, () =>
             {
@@ -57,6 +58,13 @@ public class Customization : MonoBehaviour
             foreach (var button in canvas.GetComponentsInChildren<Button>())
                 Tools.Destroy(button.gameObject.AddComponent<ShopButton>()); // hacky
         });
+
+        preview = Instantiate(ModAssets.Preview, preview).transform;
+        preview.localPosition = new(0f, -80f, -40f);
+        preview.localRotation = Quaternion.Euler(0f, 180f, 0f);
+        preview.localScale = Vector3.one * 80f;
+        preview = preview.Find("V3/Suits");
+
         Rebuild();
     }
 
@@ -77,7 +85,18 @@ public class Customization : MonoBehaviour
     {
         selection.localPosition = selection.localPosition with { x = second ? 111f : -111f };
 
-        // TODO update preview
+        #region preview
+
+        foreach (Transform suit in preview) suit.gameObject.SetActive(false);
+
+        int hat = Shop.Entries[Shop.SelectedHat].hierarchyId;
+        if (hat != -1) preview.GetChild(hat).gameObject.SetActive(true);
+
+        int jacket = Shop.Entries[Shop.SelectedJacket].hierarchyId;
+        if (jacket != -1) preview.GetChild(jacket).gameObject.SetActive(true);
+
+        #endregion
+        #region buttons
 
         int offset = second ? Shop.FirstJacket : 0;
 
@@ -98,5 +117,7 @@ public class Customization : MonoBehaviour
 
         int l = second ? Shop.SelectedJacket - offset : Shop.SelectedHat;
         UIB.Image("Selection", buttons, Shp(20f + l % 6 * 40f, -20f - l / 6 * 40f), shopc, fill: false).transform.localPosition += Vector3.back * 15f;
+
+        #endregion
     }
 }
