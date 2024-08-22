8e6d5e9f1a6e9b371ceca32f6ba2a4661a1db17a
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2024-08-22 15:01:35 +0300

    increase angle limit of shops

--- src/Jaket/Patches/DiscordPatch.cs
+++ src/Jaket/Patches/DiscordPatch.cs
@@ -33,5 +33,8 @@ public class DiscordPatch
 
         // add a customization element to allow players to choose their appearance
         if (__instance.transform.up.x == 0 && __instance.transform.up.z == 0) __instance.transform.Find("Canvas").gameObject.AddComponent<Customization>();
+
+        // simplify the use of the customization element
+        Tools.Set("angleLimit", __instance, 90f);
     }
 }
