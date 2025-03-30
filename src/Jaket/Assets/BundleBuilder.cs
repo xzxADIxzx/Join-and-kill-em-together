5de7873dbd8c1af2a65c3fcf945b839d4fe07fb9
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2025-03-30 20:05:01 +0300

    update the bundle builder script

--- src/Jaket/Assets/BundleBuilder.cs
+++ src/Jaket/Assets/BundleBuilder.cs
@@ -3,20 +3,20 @@
 using System.IO;
 using UnityEditor;
 
-/// <summary> Class for creating bundle with player doll prefab. </summary>
+/// <summary> utility for packing the required assets into bundles. </summary>
 public class BundleBuilder
 {
-    /// <summary> Directory where all bundles will be stored. </summary>
+    /// <summary> Directory to store bundles in. </summary>
     public const string BundleDir = "Assets/Bundles";
 
-    /// <summary> Creates a bundle and saves it in the bundles folder. </summary>
-    [MenuItem("Assets/Build Bundles")]
-    public static void BuildBundles()
+    /// <summary> Builds a bundle and saves it in the corresponding directory. </summary>
+    [MenuItem("Assets/Build Bundle")]
+    public static void BuildBundle()
     {
         // create a directory if it doesn't already exist
         if (!Directory.Exists(BundleDir)) Directory.CreateDirectory(BundleDir);
 
-        // build bundles for further loading from RemotePlayer
+        // build a bundle for further loading by the mod
         BuildPipeline.BuildAssetBundles(BundleDir, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
     }
 }
