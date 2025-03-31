4b85d949c3865ffc269526a1aa6fa11cfe18c180
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2025-03-31 20:42:02 +0300

    update the bar

--- src/Jaket/UI/Lib/Bar.cs
+++ src/Jaket/UI/Lib/Bar.cs
@@ -1,6 +1,11 @@
 namespace Jaket.UI.Lib;
 
 using UnityEngine;
+using UnityEngine.UI;
+
+using Jaket.Content;
+
+using static Pal;
 
 /// <summary> Either horizontal or vertical bar that gets filled with interface elements. </summary>
 public class Bar : MonoBehaviour
@@ -39,6 +44,8 @@ public class Bar : MonoBehaviour
         };
     }
 
+    #region base
+
     /// <summary> Resolves the given size of an element and returns a rect to build the element in. </summary>
     public RectTransform Resolve(string name, float size)
     {
@@ -56,4 +63,37 @@ public class Bar : MonoBehaviour
         filled += size / (voh ? -2f : 2f);
         return result;
     }
+
+    /// <summary> Adds a label with the given text, simple yet elegant. </summary>
+    public Text Text(string text, float spc, int size = 24, Color? color = null, TextAnchor align = TextAnchor.MiddleCenter) =>
+        Builder.Text(Resolve("Text", spc), text, size, color ?? white, align);
+
+    #endregion
+    #region button
+
+    /// <summary> Adds a text button, the most basic kind of buttons. </summary>
+    public Button TextButton(string text, Color? color = null, TextAnchor align = TextAnchor.MiddleCenter, Runnable callback = null) =>
+        Builder.TextButton(Resolve("TextButton", 40f), Tex.Large, color ?? white, text, 24, align, callback);
+
+    /// <summary> Adds a text button, but it's filled with the color. </summary>
+    public Button FillButton(string text, Color color, Runnable callback = null) =>
+        Builder.TextButton(Resolve("FillButton", 40f), Tex.Fill, color, text, 24, TextAnchor.MiddleCenter, callback);
+
+    /// <summary> Adds a text button, but it's filled with the color of the given team. </summary>
+    public Button TeamButton(Team team, Runnable callback = null) =>
+        Builder.TextButton(Resolve("TeamButton", 80f), Tex.Fill, team.Color(), team == Team.Pink ? "UwU" : "", 24, TextAnchor.MiddleCenter, callback);
+
+    /// <summary> Adds a button that corresponds to the style of Discord. </summary>
+    public Button DiscordButton(string text) =>
+        FillButton(text, discord, () => Application.OpenURL("https://discord.com/servers/join-and-kill-em-together-1132614140414935070"));
+
+    /// <summary> Adds a button that corresponds to the style of PayPal. </summary>
+    public Button PayPalButton(string text) =>
+        FillButton(text, paypal, () => Application.OpenURL("https://www.paypal.com/donate/?hosted_button_id=U5T68JC5LWEMU"));
+
+    /// <summary> Adds a button that corresponds to the style of Buy Me a Coffee. </summary>
+    public Button BMaCButton(string text) =>
+        FillButton(text, bmac, () => Application.OpenURL("https://www.buymeacoffee.com/adithedev"));
+
+    #endregion
 }
