1e89421efd72aabb8987e153aba213ebddc8d867
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2025-04-03 22:38:54 +0300

    add methods for building bars in fragment

--- src/Jaket/UI/Lib/Fragment.cs
+++ src/Jaket/UI/Lib/Fragment.cs
@@ -2,6 +2,10 @@ namespace Jaket.UI.Lib;
 
 using UnityEngine;
 
+using ImageType = UnityEngine.UI.Image.Type;
+
+using static Pal;
+
 /// <summary> Interface fragment that contains multiple tables. </summary>
 public class Fragment
 {
@@ -12,6 +16,8 @@ public class Fragment
 
     /// <summary> Transform containing the content of the fragment. </summary>
     public Transform Content;
+    /// <summary> Vertical bar located on the left side of the fragment. </summary>
+    public Bar Sidebar;
 
     public Fragment(Transform root, string name, bool dialog, bool woh = false, Prov<bool> cond = null, Runnable hide = null)
     {
@@ -24,5 +30,25 @@ public class Fragment
 
         Check();
         Events.OnLoaded += Check;
+    }
+
+    /// <summary> Adds a bar, it is located on the left and has a constant width. </summary>
+    protected void Bar(float height, Cons<Bar> cons)
+    {
+        Sidebar ??= Component<Bar>(Builder.Rect("Sidebar", Content, new(240f, 0f, 480f, 0f, new(0f, 0f), new(0f, 1f))).gameObject, b => b.Setup(true, 16f, 16f));
+
+        var img = Sidebar.Image(Sidebar.transform.childCount == 0 ? Tex.Back : Tex.Fill, height, semi, multiplier: 2f).gameObject;
+
+        Component(img, cons);
+        Component<HudOpenEffect>(img, e => e.originalSpeed = 40f - Sidebar.transform.childCount * 8f);
+    }
+
+    /// <summary> Adds a bar, it is located in the center and has the given size. </summary>
+    protected void Bar(float width, float height, Cons<Bar> cons)
+    {
+        var img = Builder.Image(Builder.Rect("Centerbar", Content, new(width, height)), Tex.Back, semi, ImageType.Sliced, 2f).gameObject;
+
+        Component(img, cons);
+        Component<HudOpenEffect>(img, e => e.originalSpeed = 32f);
     }
 }
