15e11258fcc7eb1390db261d1af34d8835e2d0b2
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2025-04-08 19:01:01 +0300

    update the version class

--- src/Jaket/Version.cs
+++ src/Jaket/Version.cs
@@ -1,23 +1,21 @@
 namespace Jaket;
 
 using BepInEx.Bootstrap;
-using System.Linq;
-using UnityEngine;
 using UnityEngine.Networking;
 
 using Jaket.Assets;
-using Jaket.UI;
 
-using static Jaket.UI.Rect;
-
-// TODO docs
-public class Version
+/// <summary> Class that handles interactions with the repository of the mod. This includes checking for updates and fetching compatible mods. </summary>
+public static class Version
 {
-    /// <summary> Current version of the mod installed by the player. </summary>
+    /// <summary> Current version of the project. </summary>
     public const string CURRENT = "1.3.42";
-    /// <summary> Repository of the mod, where the newest version will be taken from. </summary>
+    /// <summary> Whether current build is debug. </summary>
+    public const bool DEBUG = true;
+
+    /// <summary> Repository of the project to fetch the data from. </summary>
     public const string REPO = "xzxADIxzx/Join-and-kill-em-together";
-    /// <summary> Json fragments preceding a tag and a name of the latest version of the mod. </summary>
+    /// <summary> Json fragments preceding the name and tag of the latest version. </summary>
     public const string TAG = "\"tag_name\": \"V", NAME = "\"name\": \"";
 
     public const string GITHUB_API = "https://api.github.com";
@@ -28,72 +26,50 @@ public class Version
     /// <summary> Whether at least one incompatible mod is loaded. </summary>
     public static bool HasIncompatibility;
 
-    #region version
-
-    /// <summary> Checks for updates using GitHub and notifies the player about it. </summary>
-    public static void Check4Update() => Fetch((done, result) =>
+    /// <summary> Fetches data from the given URL address. </summary>
+    public static void Fetch(string url, Cons<string> cons) => UnityWebRequest.Get(url).SendWebRequest().completed += t =>
     {
-        if (done && Parse(result, out var latest, out var name) && latest != CURRENT) Bundle.Hud("version.outdated", false, CURRENT, latest, name);
-    });
+        var r = (t as UnityWebRequestAsyncOperation).webRequest;
+        if (r.responseCode == 200) cons(r.downloadHandler.text);
+    };
 
-    /// <summary> Fetches a json file with all versions of the mod from GitHub. </summary>
-    public static void Fetch(Cons<bool, string> result)
+    /// <summary> Fetches the latest version from GitHub and notifies players about it so that they no longer whine to me. </summary>
+    public static void Check4Updates()
     {
-        Log.Info("Checking for updates...");
-        var request = UnityWebRequest.Get($"{GITHUB_API}/repos/{REPO}/releases");
-        request.SendWebRequest().completed += _ => result(request.isDone, request.downloadHandler.text);
+        Log.Info("[UPDT] Checking for updates...");
+        Fetch($"{GITHUB_API}/repos/{REPO}/releases/latest", res =>
+        {
+            int tagIndex = res.IndexOf(TAG), nameIndex = res.IndexOf(NAME);
+            if (tagIndex == -1 || nameIndex == -1)
+            {
+                Log.Warning("[UPDT] Couldn't extract the latest version of the project.");
+                return;
+            }
+
+            var latest = res.Substring(tagIndex += TAG.Length, res.IndexOf('"', tagIndex) - tagIndex);
+            var name = res.Substring(nameIndex += NAME.Length, res.IndexOf('"', nameIndex) - nameIndex);
+
+            if (latest != CURRENT) Bundle.Hud("version.outdated", false, CURRENT, latest, name);
+        });
     }
 
-    /// <summary> Extracts the tag and name of the latest version of the mod from the given json file. </summary>
-    public static bool Parse(string result, out string latest, out string name)
-    {
-        latest = name = "Failed to parse data ;(";
-
-        int tagIndex = result.IndexOf(TAG), nameIndex = result.IndexOf(NAME);
-        if (tagIndex == -1 || nameIndex == -1) return false;
-
-        latest = result.Substring(tagIndex += TAG.Length, result.IndexOf('"', tagIndex) - tagIndex);
-        name = result.Substring(nameIndex += NAME.Length, result.IndexOf('"', nameIndex) - nameIndex);
-
-        return true;
-    }
-
-    /// <summary> Adds the mod version to the bottom left edge of the screen. </summary>
-    public static void Label(Transform parent)
-    {
-        var r = Blw(36f, 40f);
-        UIB.Table("Version", parent, r, table => UIB.Text($"Jaket version is {CURRENT}", table, r.Text, Color.grey));
-    }
-
-    #endregion
-    #region compatibility
-
-    /// <summary> Fetches a json file with all mods that are compatible with Jaket. </summary>
+    /// <summary> Fetches the list of mods that are compatible with Jaket from GitHub. </summary>
     public static void FetchCompatible()
     {
-        Log.Info("Fetching the list of compatible mods...");
-        var request = UnityWebRequest.Get($"{GITHUB_RAW}/{REPO}/refs/heads/main/compatible-mods.json");
-        request.SendWebRequest().completed += _ =>
+        Log.Info("[UPDT] Fetching the list of compatible mods...");
+        Fetch($"{GITHUB_RAW}/{REPO}/refs/heads/main/compatible-mods.json", res =>
         {
-            if (request.isDone && (request.responseCode == 0 || request.responseCode == 200)) Parse(request.downloadHandler.text);
+            Compatible = new string[res.Count(c => c == '"') / 2];
 
-            HasIncompatibility = Chainloader.PluginInfos.Values.Any(info => !Compatible.Contains(info.Metadata.Name));
-        };
+            for (int i = 0, s = 0, e = 0; i < Compatible.Length; i++)
+            {
+                s = res.IndexOf('"', e + 1) + 1;
+                e = res.IndexOf('"', s);
+
+                Compatible[i] = res[s..e];
+            }
+
+            HasIncompatibility = Compatible.Any(Chainloader.PluginInfos.ContainsKey);
+        });
     }
-
-    /// <summary> Extracts the list of compatible mods from the given json file. </summary>
-    public static void Parse(string result)
-    {
-        Compatible = new string[result.Count(c => c == '\n') - 2];
-
-        for (int i = 0, s = 0, e = 0; i < Compatible.Length; i++)
-        {
-            s = result.IndexOf('"', e + 1) + 1;
-            e = result.IndexOf('"', s);
-
-            Compatible[i] = result.Substring(s, e - s);
-        }
-    }
-
-    #endregion
 }
