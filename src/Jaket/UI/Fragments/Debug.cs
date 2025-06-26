1431b9c2a7ff20f732884e4bd362ab2601f0fcae
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2025-06-26 20:02:57 +0300

    add graphs to display stats

--- src/Jaket/UI/Fragments/Debug.cs
+++ src/Jaket/UI/Fragments/Debug.cs
@@ -15,7 +15,7 @@ public class Debug : Fragment
     /// <summary> Warehouses containing a lot of diverse information. </summary>
     private Data readBs, sentBs, readMs, writeMs, entityMs, targetMs, totalMs, flushMs;
 
-    public Debug(Transform root) : base(root, "Debug", false) => Component<Bar>(Rect("Content", new(0f, 190f, 1920f, 380f, new(.5f, 0f))).gameObject, b =>
+    public Debug(Transform root) : base(root, "Debug", false) => Component<Bar>(Rect("Content", new(0f, 190f, 1920f, 440f, new(.5f, 0f))).gameObject, b =>
     {
         b.Setup(true, 16f, 16f);
         b.Subbar(136f, s =>
@@ -25,19 +25,36 @@ public class Debug : Fragment
             Component<Bar>(s.Image(Tex.Fill, 320f, semi, multiplier: 2).gameObject, b =>
             {
                 b.Setup(true);
-                b.Text("BYTES READ   ", 24f, out (readBs   = new(19)).Label, color: green);
-                b.Text("BYTES SENT   ", 24f, out (sentBs   = new(19)).Label, color: Darker(green));
-                b.Text("READ TIME    ", 24f, out (readMs   = new(96)).Label, color: orange);
-                b.Text("WRITE TIME   ", 24f, out (writeMs  = new(96)).Label, color: Darker(orange));
+                b.Text("BYTES READ   ", 24f, out (readBs   = new(20)).Label, color: green);
+                b.Text("BYTES SENT   ", 24f, out (sentBs   = new(20)).Label, color: Darker(green));
+                b.Text("READ TIME    ", 24f, out (readMs   = new(97)).Label, color: orange);
+                b.Text("WRITE TIME   ", 24f, out (writeMs  = new(97)).Label, color: Darker(orange));
             });
             Component<Bar>(s.Image(Tex.Fill, 320f, semi, multiplier: 2).gameObject, b =>
             {
                 b.Setup(true);
-                b.Text("ENTITY UPDATE", 24f, out (entityMs = new(96)).Label, color: blue);
-                b.Text("TARGET UPDATE", 24f, out (targetMs = new(96)).Label, color: Darker(blue));
-                b.Text("TOTAL TIME   ", 24f, out (totalMs  = new(96)).Label, color: purple);
-                b.Text("FLUSH TIME   ", 24f, out (flushMs  = new(96)).Label, color: Darker(purple));
+                b.Text("ENTITY UPDATE", 24f, out (entityMs = new(97)).Label, color: blue);
+                b.Text("TARGET UPDATE", 24f, out (targetMs = new(97)).Label, color: Darker(blue));
+                b.Text("TOTAL TIME   ", 24f, out (totalMs  = new(97)).Label, color: purple);
+                b.Text("FLUSH TIME   ", 24f, out (flushMs  = new(97)).Label, color: Darker(purple));
             });
+        });
+        b.Subbar(256f, s =>
+        {
+            s.Setup(false, 0f, 16f);
+
+            void Build(Image graph, Data data) => Component<UILineRenderer>(Builder.Rect("Graph", graph.transform, new(8f, 8f, 0f, 0f, new())).gameObject, g => data.Graph = g);
+            var byteGraph = s.Image(Tex.Fill, 320f,  semi, multiplier: 2);
+            var timeGraph = s.Image(Tex.Fill, 1552f, semi, multiplier: 2);
+
+            Build(byteGraph, readBs);
+            Build(byteGraph, sentBs);
+            Build(timeGraph, readMs);
+            Build(timeGraph, writeMs);
+            Build(timeGraph, entityMs);
+            Build(timeGraph, targetMs);
+            Build(timeGraph, totalMs);
+            Build(timeGraph, flushMs);
         });
     });
 
@@ -100,8 +117,9 @@ public class Debug : Fragment
         {
             var x = -16f;
             var o = new Vector2[data.Length];
-            for (int i = 0; i < data.Length; i++) o[i] = new(x += 16f, data[(start + i) % data.Length] / peak);
+            for (int i = 0; i < data.Length; i++) o[i] = new(x += 16f, data[(start + i) % data.Length] / peak * 240f);
 
+            Graph.color = Label.color;
             Graph.Points = o;
         }
 
