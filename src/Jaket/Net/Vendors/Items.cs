4ee49de4b9d521e9b8e6e520403f38c2200ac737
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2025-09-05 23:50:26 +0300

    fixes

--- src/Jaket/Net/Vendors/Items.cs
+++ src/Jaket/Net/Vendors/Items.cs
@@ -25,10 +25,10 @@ public class Items : Vendor
 
         GameAssets.Prefab("Fishing/Fish Pickup Template.prefab", p => FishTemplate = p);
 
-        Vendor.Prefabs[(byte)EntityType.V2       ] = ModAssets.V2;
-        Vendor.Prefabs[(byte)EntityType.V3       ] = ModAssets.V3;
-        Vendor.Prefabs[(byte)EntityType.xzxADIxzx] = ModAssets.xzxADIxzx;
-        Vendor.Prefabs[(byte)EntityType.Sowler   ] = ModAssets.Sowler;
+        Events.Post(() => ModAssets.V2        != null, () => Vendor.Prefabs[(byte)EntityType.V2       ] = ModAssets.V2       );
+        Events.Post(() => ModAssets.V3        != null, () => Vendor.Prefabs[(byte)EntityType.V3       ] = ModAssets.V3       );
+        Events.Post(() => ModAssets.xzxADIxzx != null, () => Vendor.Prefabs[(byte)EntityType.xzxADIxzx] = ModAssets.xzxADIxzx);
+        Events.Post(() => ModAssets.Sowler    != null, () => Vendor.Prefabs[(byte)EntityType.Sowler   ] = ModAssets.Sowler   );
 
         for (EntityType i = EntityType.SkullBlue; i <= EntityType.BaitFace;  i++) Vendor.Suppliers[(byte)i] = (id, type) => new CommonItem(id, type);
         for (EntityType i = EntityType.FishFunny; i <= EntityType.FishBurnt; i++) Vendor.Suppliers[(byte)i] = (id, type) => new Fish      (id, type);
