441cab51fd335dd57ffe27053e3b8fff93b4cee4
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2025-09-16 20:03:36 +0300

    synchronize baits

--- src/Jaket/Net/Types/Items/Fish.cs
+++ src/Jaket/Net/Types/Items/Fish.cs
@@ -31,7 +31,7 @@ public class Fish : Item
 
     [HarmonyPatch(typeof(FishCooker), "OnTriggerEnter")]
     [HarmonyPrefix]
-    static bool Cook(FishCooker __instance, Collider other, bool ___unusable)
+    static bool Cook(Collider other, bool ___unusable)
     {
         var agent = other.GetComponentInParent<Agent>();
         if (agent && Scene == "Level 5-S")
@@ -40,17 +40,42 @@ public class Fish : Item
             {
                 if (___unusable)
                 {
-                    if (!HudMessageReceiver.Instance.text.enabled) Bundle.Hud("fish.too-small");
+                    Bundle.Hud("fish.too-small");
                     return false;
                 }
                 bool valid = i is Fish f && f.fish.fishObject.canBeCooked;
-                if (!valid) Bundle.Hud("fish.failure");
+                if (!valid) Bundle.Hud("fish.fail");
 
                 var result = Entities.Items.Make(valid ? EntityType.FishCooked : EntityType.FishBurnt, other.transform.position);
                 if (result.TryGetComponent(out Rigidbody rb))
                     rb.velocity = (NewMovement.Instance.transform.position - other.transform.position).normalized * 18f + Vector3.up * 10f;
 
                 i.Kill(2, w => { w.Bool(false); w.Bool(true); });
+            }
+            return false;
+        }
+        else return true;
+    }
+
+    [HarmonyPatch(typeof(BaitItem), "OnTriggerEnter")]
+    [HarmonyPrefix]
+    static bool Bait(Collider other, BaitItem __instance, FishDB[] ___supportedWaters, FishObject[] ___attractFish, GameObject ___consumedPrefab, bool ___silentFail)
+    {
+        var agent = __instance.GetComponentInParent<Agent>();
+        if (agent && Scene == "Level 5-S")
+        {
+            if (agent.Patron is Item i && i.IsOwner && other.TryGetComponent(out Water w) && w.fishDB)
+            {
+                if (___supportedWaters.Any(s => s == w.fishDB))
+                {
+                    Bundle.Hud("bait.took");
+
+                    w.attractFish = ___attractFish;
+                    Inst(___consumedPrefab, __instance.transform.position);
+
+                    i.Kill();
+                }
+                else if (!___silentFail) Bundle.Hud("bait.nope");
             }
             return false;
         }
