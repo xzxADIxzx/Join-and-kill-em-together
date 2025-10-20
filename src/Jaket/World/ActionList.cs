f8d5142c5cd25a9cc04c877a8c792c5dc498087a
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2025-10-20 18:09:57 +0300

    add regions for the new action list

--- src/Jaket/World/ActionList.cs
+++ src/Jaket/World/ActionList.cs
@@ -26,5 +26,145 @@ public static class ActionList
     public static void Load()
     {
         string l; // decreases the size of further lines
+        #region 0-1
+        l = "Level 0-1";
+
+        #endregion
+        #region 0-2
+        l = "Level 0-2";
+
+        #endregion
+        #region 0-3
+        l = "Level 0-3";
+
+        #endregion
+        #region 0-4
+        l = "Level 0-4";
+
+        #endregion
+        #region 0-5
+        l = "Level 0-5";
+
+        #endregion
+        #region 0-S
+        l = "Level 0-S";
+
+        #endregion
+        #region 1-1
+        l = "Level 1-1";
+
+        #endregion
+        #region 1-2
+        l = "Level 1-2";
+
+        #endregion
+        #region 1-3
+        l = "Level 1-3";
+
+        #endregion
+        #region 1-4
+        l = "Level 1-4";
+
+        #endregion
+        #region 1-S
+        l = "Level 1-S";
+
+        #endregion
+        #region 2-1
+        l = "Level 2-1";
+
+        #endregion
+        #region 2-2
+        l = "Level 2-2";
+
+        #endregion
+        #region 2-3
+        l = "Level 2-3";
+
+        #endregion
+        #region 2-4
+        l = "Level 2-4";
+
+        #endregion
+        #region 2-S
+        l = "Level 2-S";
+
+        #endregion
+        #region 3-1
+        l = "Level 3-1";
+
+        #endregion
+        #region 3-2
+        l = "Level 3-2";
+
+        #endregion
+        #region 4-1
+        l = "Level 4-1";
+
+        #endregion
+        #region 4-2
+        l = "Level 4-2";
+
+        #endregion
+        #region 4-3
+        l = "Level 4-3";
+
+        #endregion
+        #region 4-4
+        l = "Level 4-4";
+
+        #endregion
+        #region 4-S
+        l = "Level 4-S";
+
+        #endregion
+        #region 5-1
+        l = "Level 5-1";
+
+        #endregion
+        #region 5-2
+        l = "Level 5-2";
+
+        #endregion
+        #region 5-3
+        l = "Level 5-3";
+
+        #endregion
+        #region 5-4
+        l = "Level 5-4";
+
+        #endregion
+        #region 5-S
+        l = "Level 5-S";
+
+        #endregion
+        #region 6-1
+        l = "Level 6-1";
+
+        #endregion
+        #region 6-2
+        l = "Level 6-2";
+
+        #endregion
+        #region 7-1
+        l = "Level 7-1";
+
+        #endregion
+        #region 7-2
+        l = "Level 7-2";
+
+        #endregion
+        #region 7-3
+        l = "Level 7-3";
+
+        #endregion
+        #region 7-4
+        l = "Level 7-4";
+
+        #endregion
+        #region 7-S
+        l = "Level 7-S";
+
+        #endregion
     }
 }
