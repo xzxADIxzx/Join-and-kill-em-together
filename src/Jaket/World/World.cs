fe2f1b72b08caee1844364622eb6b5a0ed9bb6d4
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2025-10-22 22:38:45 +0300

    increase the size of the performed actions' argument storage

--- src/Jaket/World/World.cs
+++ src/Jaket/World/World.cs
@@ -6,13 +6,13 @@ using Jaket.Content;
 using Jaket.IO;
 using Jaket.Net;
 
-/// <summary> Class that manages objects in the level, such as hook points, skull cases, triggers and etc. </summary>
+/// <summary> Class responsible for managing world objects and actions. </summary>
 public class World
 {
     /// <summary> Actions that were performed previously. </summary>
-    private static bool[] performed = new bool[byte.MaxValue + 1];
+    private static bool[] performed = new bool[(byte.MaxValue + 1)];
     /// <summary> Vectors that were arguments of actions. </summary>
-    private static Vector2[] pos = new Vector2[byte.MaxValue + 1];
+    private static Vector2[] pos = new Vector2[(byte.MaxValue + 1) * 16];
 
     /// <summary> Subscribes to several events for proper work. </summary>
     public static void Load()
@@ -32,7 +32,14 @@ public class World
 
             ActionList.Each(a => !a.Dynamic, a => a.Perform(default));
             int counter = 0;
-            ActionList.Each(a => performed[counter++] && a.Dynamic, a => a.Perform(pos[counter - 1]));
+            ActionList.Each(a => performed[counter++] && a.Dynamic, a =>
+            {
+                for (int i = 0; i < 16; i++)
+                {
+                    var pi = pos[counter - 1 + i];
+                    if (pi != default) a.Perform(pi);
+                }
+            });
         }
         Events.OnLoad += Restore;
         Events.OnLobbyEnter += Restore;
@@ -44,7 +51,7 @@ public class World
     #region data
 
     /// <summary> Number of bytes that the world data takes in a snapshot. </summary>
-    public static int BufferSize => 4 + ((Pending ?? Scene).Length + Version.CURRENT.Length) * 2 + performed.Count(b => b) * 13;
+    public static int BufferSize => 4 + (Pending ?? Scene).Length + Version.CURRENT.Length + performed.Count(b => b) * 2 + pos.Count(p => p != default) * 8;
 
     /// <summary> Writes the world data into a snapshot. </summary>
     public static void WriteData(Writer w)
@@ -55,10 +62,14 @@ public class World
         w.Byte((byte)PrefsManager.Instance.GetInt("difficulty"));
         w.Byte((byte)performed.Count(b => b));
 
-        for (int i = 0; i < performed.Length; i++) if (performed[i])
+        for (int i = 0, j = 0, k = 0; i < performed.Length; i++) if (performed[i])
         {
+            for (j = 0; j < 16; j++) if (pos[i + j] == default) { k = j; break; }
+
             w.Byte((byte)i);
-            w.Vector(pos[i]);
+            w.Byte((byte)k);
+
+            for (j = 0; j < k; j++) { w.Float(pos[i + j].x); w.Float(pos[i + j].y); }
         }
     }
 
@@ -77,11 +88,13 @@ public class World
         {
             PrefsManager.Instance.SetInt("difficulty", r.Byte());
 
-            for (byte i = r.Byte(); i > 0; i--)
+            for (int w = r.Byte(); w > 0; w--)
             {
-                byte id = r.Byte();
-                performed[id] = true;
-                pos[id] = r.Vector();
+                byte i = r.Byte();
+                byte k = r.Byte();
+
+                performed[i] = true;
+                for (int j = 0; j < k; j++) pos[i + j] = new(r.Float(), r.Float());
             }
         }
     }
