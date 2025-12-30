14419446a977bae5fd1e8f01149253f5b68dd537
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2025-12-30 16:25:58 +0300

    enhance the build script

--- build.sh
+++ build.sh
@@ -1,36 +1,84 @@
 #!/bin/bash
+# region constants
 
-b="\033[1;32m"
+r="\033[1;31m"
+g="\033[1;32m"
 n="\033[0;39m"
 
-project="Jaket"
+function faulty
+{
+    echo -e "$r!> $1$n"
+}
 
+function region
+{
+    echo -e "$g=> $1$n"
+}
 
+# endregion
+# region help
 
-[ "$1" != "release" ] && [ "$1" != "debug" ] && echo -e "Use either$b release$n or$b debug$n as an argument" && exit 1
-
-if [ "$1" == "release" ]
-
-
-
+if echo $1 | grep -q "h" || [ $# -lt 1 ]
 then
-    echo -e "=>$b Building release dll...$n"
-    dotnet build $project.csproj --configuration Release
-else
-    echo -e "=>$b Building debug dll...$n"
-    dotnet build $project.csproj
+    echo "Builds the project"
+    echo ""
+    echo "Usage: build <operations> [deploy-directory]"
+    echo "Operations:"
+    echo "    -h display this text"
+    echo "    -r use release configuration"
+    echo "    -d deploy to a directory"
+    echo "    -i ignore the path file"
+    exit 0
 fi
 
-# make sure to not commit changes performed in this file
-git update-index --skip-worktree Path.props
+# endregion
+# region build
 
+if echo $1 | grep -q "r"
+then
+    region "Building release version..."
+    dotnet build Jaket.csproj --configuration Release
+else
+    region "Building debug version..."
+    dotnet build Jaket.csproj
+fi
 
+# endregion
+# region deploy
 
-[ "$2" != "deploy" ] && exit 0
+if echo $1 | grep -q "d"
+then
+    region "Deploying the built version..."
 
-echo -e "=>$b Deploying the builded project...$n"
+    if [ $# -lt 2 ]
+    then
+        faulty "Target directory was not provided"
+        exit 1
+    fi
 
+    location=$(echo $1 | grep -q "r" && echo Release || echo Debug)
 
+    cp bin/$location/netstandard2.1/Jaket.dll $2
+    cp assets/assets.bundle $2
+    cp assets/bundles/*.properties $2
+fi
 
-mv bin/Debug/netstandard2.1/$project.dll $3
-cp assets/bundles/*.properties $3
+# endregion
+# region ignore
+
+if echo $1 | grep -q "i"
+then
+    region "Ignoring the Path.props file..."
+
+    git rev-parse --is-inside-work-tree &> /dev/null
+
+    if [ $? -ne 0 ]
+    then
+        faulty "Couldn't find a repository in the work directory"
+        exit 1
+    fi
+
+    git update-index --skip-worktree Path.props
+fi
+
+# endregion
