fd24c21777b7f63c69d4ad29c97a989c90f22f66
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2025-12-31 18:15:45 +0300

    add gamemodes

---
+++ src/Jaket/Content/Gamemode.cs
@@ -0,0 +1,13 @@
+namespace Jaket.Content;
+
+/// <summary> All gamemodes. Will replenish over time. </summary>
+public enum Gamemode : byte
+{
+    Campaign,
+    Manhunt,
+    Versus,
+    Arena,
+    ArmsRace,
+    PaintTheWorld,
+    HideAndSeek,
+}
