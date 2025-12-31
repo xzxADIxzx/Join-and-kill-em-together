633a8952b6538d4916fd6be7cf2846344f4b17ca
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2025-12-31 18:54:10 +0300

    add gamemodes options

--- src/Jaket/Content/Gamemode.cs
+++ src/Jaket/Content/Gamemode.cs
@@ -11,3 +11,54 @@ public enum Gamemode : byte
     PaintTheWorld,
     HideAndSeek,
 }
+
+/// <summary> Set of different tools for working with gamemodes. </summary>
+public static class Gamemodes
+{
+    /// <summary> List of all of the gamemodes that is used for iterating. </summary>
+    public static Gamemode[] All = { Gamemode.Campaign, Gamemode.Manhunt, Gamemode.Versus, Gamemode.Arena, Gamemode.ArmsRace, Gamemode.PaintTheWorld, Gamemode.HideAndSeek };
+
+    /// <summary> Whether the gamemode implies a deadly competition between all teams. </summary>
+    public static bool PvP(this Gamemode gm) => gm switch
+    {
+        Gamemode.Versus        => true,
+        Gamemode.Arena         => true,
+        Gamemode.ArmsRace      => true,
+        Gamemode.HideAndSeek   => true,
+        _                      => false,
+    };
+
+    /// <summary> Whether the gamemode implies team based health points. </summary>
+    public static bool HPs(this Gamemode gm) => gm switch
+    {
+        Gamemode.Versus        => true,
+        Gamemode.Arena         => true,
+        _                      => false,
+    };
+
+    /// <summary> Whether the gamemode implies healing after killing a player. </summary>
+    public static bool HealOnKill(this Gamemode gm) => gm switch
+    {
+        Gamemode.Versus        => true,
+        Gamemode.HideAndSeek   => true,
+        _                      => false,
+    };
+
+    /// <summary> Whether the gamemode implies the absence of common enemies. </summary>
+    public static bool NoCommonEnemies(this Gamemode gm) => gm switch
+    {
+        Gamemode.Versus        => true,
+        Gamemode.Arena         => true,
+        Gamemode.ArmsRace      => true,
+        Gamemode.PaintTheWorld => true,
+        Gamemode.HideAndSeek   => true,
+        _                      => false,
+    };
+
+    /// <summary> Whether the gamemode implies the presence of enemies waves. </summary>
+    public static bool WaveLikeEnemies(this Gamemode gm) => gm switch
+    {
+        Gamemode.Arena         => true,
+        _                      => false,
+    };
+}
