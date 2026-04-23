9027b6d4e744e43121fd60c1fabc9678c04bcf9c
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2026-04-23 18:16:37 +0300

    update magnet

--- src/Jaket/Net/Types/Projectiles/Magnet.cs
+++ src/Jaket/Net/Types/Projectiles/Magnet.cs
@@ -1,41 +1,56 @@
 namespace Jaket.Net.Types;
 
 using HarmonyLib;
+using ULTRAKILL.Cheats;
 using UnityEngine;
 
 using Jaket.Content;
 using Jaket.IO;
 
 /// <summary> Tangible entity of the magnet type. </summary>
-public class Magnet : OwnableEntity
+public class Magnet : Projectile
 {
     Agent agent;
     Float posX, posY, posZ, rotX, rotY, rotZ;
+    TimeBomb bomb;
 
-    public Magnet(uint id, EntityType type) : base(id, type) { }
+    /// <summary> Whether the timer is counting down. </summary>
+    private bool counting;
+
+    public Magnet(uint id, EntityType type) : base(id, type, true, true, false) { }
 
     #region snapshot
 
-    public override int BufferSize => 29;
+    public override int BufferSize => 34;
 
     public override void Write(Writer w)
     {
+        WriteOwner(ref w);
+
         if (IsOwner)
         {
             w.Vector(agent.Position);
             w.Vector(agent.Rotation);
+
+            w.Bool(!PauseTimedBombs.Paused && !NoWeaponCooldown.NoCooldown);
         }
         else
         {
             w.Floats(posX, posY, posZ);
             w.Floats(rotX, rotY, rotZ);
+
+            w.Bool(counting);
         }
     }
 
     public override void Read(Reader r)
     {
+        if (ReadOwner(ref r)) return;
+
         r.Floats(ref posX, ref posY, ref posZ);
         r.Floats(ref rotX, ref rotY, ref rotZ);
+
+        counting = r.Bool();
     }
 
     #endregion
@@ -47,11 +62,7 @@ public class Magnet : OwnableEntity
     {
         base.Assign(this.agent = agent);
 
-        agent.Get(out Rigidbody rb);
-        agent.Get(out TrailRenderer trail);
-
-        rb.isKinematic   = !IsOwner;
-        trail.startColor = (Networking.Entities[Owner] is RemotePlayer p ? p.Team : Networking.LocalPlayer.Team).Color() with { a = .4f };
+        agent.Get(out bomb);
     }
 
     public override void Update(float delta)
@@ -60,14 +71,16 @@ public class Magnet : OwnableEntity
 
         agent.Position = new(posX.GetAware(delta), posY.GetAware(delta), posZ.GetAware(delta));
         agent.Rotation = new(rotX.GetAngle(delta), rotY.GetAngle(delta), rotZ.GetAngle(delta));
-    }
 
-    public override void Damage(Reader r) { }
+        bomb.activated = counting && bomb.timer > Time.deltaTime;
+    }
 
     public override void Killed(Reader r, int left)
     {
-        Hidden = true;
-        if (agent) Dest(agent.gameObject);
+        base.Killed(r, left);
+
+        if (left >= 1 && r.Bool())
+            Inst(bomb.explosion, agent.Position);
     }
 
     #endregion
@@ -82,10 +95,10 @@ public class Magnet : OwnableEntity
 
     [HarmonyPatch(typeof(Harpoon), nameof(Harpoon.OnDestroy))]
     [HarmonyPrefix]
-    static void Death(Harpoon __instance)
+    static bool Death(Harpoon __instance) => Kill<Magnet>(__instance, e =>
     {
-        if (__instance.TryGetComponent(out Agent a) && a.Patron is Magnet m && !m.Hidden) m.Kill();
-    }
+        if (!e.Hidden) e.Kill(1, w => w.Bool(true));
+    });
 
     [HarmonyPatch(typeof(global::Magnet), nameof(global::Magnet.OnTriggerEnter))]
     [HarmonyPatch(typeof(global::Magnet), nameof(global::Magnet.OnTriggerExit))]
