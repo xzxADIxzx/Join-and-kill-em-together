d23448b049aa251bc1800b1e9d24eccb07e3e892
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2026-07-13 01:56:10 +0300

    make pointers thread-safe

--- src/Jaket/IO/Pointers.cs
+++ src/Jaket/IO/Pointers.cs
@@ -3,30 +3,15 @@ namespace Jaket.IO;
 using System;
 using System.Runtime.InteropServices;
 
-/// <summary> Set of different tools for working with the unmanaged memory. </summary>
+/// <summary> Thread-safe allocator of the unmanaged memory. </summary>
 public static class Pointers
 {
-    /// <summary> Number of bytes to allocate for the project needs. </summary>
-    public const int RESERVED = 1024 * 1024;
+    /// <summary> Number of bytes allocated for each thread. </summary>
+    public const int RESERVED = 1024;
 
-    /// <summary> Pointer to the beginning of the allocated memory. </summary>
+    [ThreadStatic]
     private static Ptr memory;
-    /// <summary> Number of bytes reserved in the allocated memory. </summary>
-    private static int offset;
 
-    /// <summary> Allocates a portion of memory for future reservation. </summary>
-    public static void Allocate() => memory = Marshal.AllocHGlobal(RESERVED);
-
-    /// <summary> Reserves the given number of bytes in the memory. </summary>
-    public static Ptr Reserve(int bytesCount)
-    {
-        offset += bytesCount;
-
-        if (offset >= RESERVED) throw new OutOfMemoryException("Caught an attempt to reserve more bytes than were allocated in the unmanaged memory");
-
-        return memory + offset - bytesCount;
-    }
-
-    /// <summary> Frees up the reserved memory. </summary>
-    public static void Free() => offset = 0;
+    /// <summary> Allocates and stores a fragment of memory. </summary>
+    public static Ptr Allocate() => memory != default ? memory : memory = Marshal.AllocHGlobal(RESERVED);
 }
