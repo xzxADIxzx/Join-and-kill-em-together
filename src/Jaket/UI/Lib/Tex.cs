216e9308c3600584ab6d12c8d863315f08b4e90d
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2026-07-19 19:11:06 +0300

    update the textures

--- src/Jaket/UI/Lib/Tex.cs
+++ src/Jaket/UI/Lib/Tex.cs
@@ -7,31 +7,31 @@ using Jaket.Assets;
 /// <summary> List of textures used to build the interface. </summary>
 public static class Tex
 {
-    /// <summary> Backgrounds with cut corners and different amount of dashes. </summary>
-    public static Sprite Fill, Back, Dash;
-    /// <summary> Borders with cut corners and different size. </summary>
-    public static Sprite Small, Large;
-    /// <summary> Horizontal and vertical slider handle. </summary>
+    /// <summary> Backgrounds. </summary>
+    public static Sprite Fill, Back;
+    /// <summary> Borders. </summary>
+    public static Sprite BrdS, BrdL;
+    /// <summary> Handles. </summary>
     public static Sprite Hort, Vert;
-    /// <summary> Checkmark sprite for the checkbox, and mask sprite for the slider filler. </summary>
-    public static Sprite Mark, Mask;
-    /// <summary> Hollow circle and circular shadow. </summary>
-    public static Sprite Circle, Shadow;
-    /// <summary> I see you and muzzle flash. </summary>
-    public static Sprite Dead, Flash;
+    /// <summary> Stripes. </summary>
+    public static Sprite Dash, Mask;
+    /// <summary> Checkmark. </summary>
+    public static Sprite Mark;
+    /// <summary> Common. </summary>
+    public static Sprite Circle, Shadow, Dead, Flash;
 
-    /// <summary> Loads the textures from memory. </summary>
+    /// <summary> Loads the textures. </summary>
     public static void Load()
     {
         GameAssets.Sprite("Controls/Round_FillLarge.png",           s => Fill   = s);
         GameAssets.Sprite("Controls/Round_VertHandle_Invert 1.png", s => Back   = s);
-        GameAssets.Sprite("Controls/StripesMaskSM.png",             s => Dash   = s);
-        GameAssets.Sprite("Controls/Round_BorderSmall.png",         s => Small  = s);
-        GameAssets.Sprite("Controls/Round_BorderLarge.png",         s => Large  = s);
+        GameAssets.Sprite("Controls/Round_BorderSmall.png",         s => BrdS   = s);
+        GameAssets.Sprite("Controls/Round_BorderLarge.png",         s => BrdL   = s);
         GameAssets.Sprite("Controls/Round_HorizHandle_Invert.png",  s => Hort   = s);
         GameAssets.Sprite("Controls/Round_VertHandle_Invert.png",   s => Vert   = s);
-        GameAssets.Sprite("Controls/Check.png",                     s => Mark   = s);
+        GameAssets.Sprite("Controls/StripesMaskSM.png",             s => Dash   = s);
         GameAssets.Sprite("Controls/Round_SliderFill.png",          s => Mask   = s);
+        GameAssets.Sprite("Controls/Check.png",                     s => Mark   = s);
         GameAssets.Sprite("circle.png",                             s => Circle = s);
         GameAssets.Sprite("weaponwheelbackground.png",              s => Shadow = s);
         GameAssets.Sprite("ISeeYou.png",                            s => Dead   = s);
@@ -39,8 +39,19 @@ public static class Tex
     }
 
     /// <summary> Executes the task after loading sprites. </summary>
-    public static void OnLoad(Runnable task) => Events.Post(() => Fill & Back & Dash & Small & Large & Hort & Vert & Mark & Mask & Circle & Shadow & Dead, task);
+    public static void OnLoad(Runnable task) => Events.Post(() => Fill & Back & BrdS & BrdL & Hort & Vert & Dash & Mask & Mark & Circle & Shadow & Dead & Flash, task);
 
-    /// <summary> Returns the scale of the given sprite. </summary>
-    public static float Scale(Sprite sprite) => sprite == Mask ? 5f : new[] { Fill, Back, Dash, Small, Large, Vert }.Has(sprite) ? 4f : 1f;
+    /// <summary> Returns the default scale of the sprite. </summary>
+    public static float Scale(Sprite sprite) => sprite switch
+    {
+        _ when sprite == Fill => 4.0f,
+        _ when sprite == Back => 4.0f,
+        _ when sprite == BrdS => 4.0f,
+        _ when sprite == BrdL => 4.0f,
+        _ when sprite == Hort => 4.4f,
+        _ when sprite == Vert => 4.4f,
+        _ when sprite == Dash => 2.0f,
+        _ when sprite == Mask => 5.3f,
+        _                     => 1.0f,
+    };
 }
