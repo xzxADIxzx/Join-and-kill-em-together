e6cb3aa515dc0974d0e8a0565dcca486bdbc9bb7
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2026-08-11 23:24:15 +0300

    clean packet types

--- src/Jaket/Content/PacketType.cs
+++ src/Jaket/Content/PacketType.cs
@@ -5,24 +5,20 @@ public enum PacketType : byte
 {
     /// <summary> Initiates loading of the level selected by the lobby owner. </summary>
     Level,
-    /// <summary> Dear client, could you leave the lobby please? Cheers~ :heart: </summary>
-    Ban,
 
-    /// <summary> Contains data of a particular entity. </summary>
+    /// <summary> Contains data of an entity. </summary>
     Snapshot,
-    /// <summary> Contains data of a hitscan shot. </summary>
+    /// <summary> Contains data of a hitscan. </summary>
     Hitscan,
-
-    /// <summary> Contains data of a dealt damage unit. </summary>
+    /// <summary> Contains data of a fraction of dealt damage. </summary>
     Damage,
-    /// <summary> Contains data of a death and some entity-specific flags. </summary>
+    /// <summary> Contains data of an entity's death and bits. </summary>
     Death,
 
     /// <summary> A player changed their look. </summary>
     Style,
     /// <summary> A player punched or parried. </summary>
     Punch,
-
     /// <summary> A player pointed somewhere. </summary>
     Point,
     /// <summary> A player sprayed something. </summary>
@@ -37,7 +33,4 @@ public enum PacketType : byte
     WorldAction,
     /// <summary> Any kind of interaction with the Cyber Grind. </summary>
     CyberAction,
-
-    /// <summary> Player voted for an option. Voting can be different: skip of a cutscene, dialog or choice of an option. </summary>
-    Vote
 }
