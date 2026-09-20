d4f4aac6246d5a2c41593c4dba7a0b9cc00593cf
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2026-09-20 16:14:32 +0300

    fix null reference in commands

--- src/Jaket/Commands/Commands.cs
+++ src/Jaket/Commands/Commands.cs
@@ -33,8 +33,7 @@ public static class Commands
             }
             UI.Chat.Receive(string.Join('\n', content));
         }
-
-        var chat = UI.Chat;
+        static void Receive(string content) => UI.Chat.Receive(content);
 
         Handler.Register("help", "Display the list of all commands", args =>
         {
@@ -48,18 +47,18 @@ public static class Commands
             Print(content);
         });
 
-        Handler.Register("hello", "Resend the tips for new players", args => chat.SayHello());
+        Handler.Register("hello", "Resend the tips for new players", args => UI.Chat.SayHello());
 
         Handler.Register("tts-volume", "\\[0-100]", "Set the volume of TTS", args =>
         {
             if (args.Length == 0)
-                chat.Receive($"[orange]TTS volume is {Settings.Volume}.");
+                Receive($"[orange]TTS volume is {Settings.Volume}.");
 
             else if (int.TryParse(args[0], out int value))
-                chat.Receive($"[green]TTS volume is set to {Settings.Volume = Mathf.Clamp(value, 0, 100)}.");
+                Receive($"[green]TTS volume is set to {Settings.Volume = Mathf.Clamp(value, 0, 100)}.");
 
             else
-                chat.Receive("[red]Couldn't parse the value, it must be an integer between 0 and 100.");
+                Receive("[red]Couldn't parse the value, it must be an integer between 0 and 100.");
         });
 
         Handler.Register("tts", "\\[on/off]", "Toggle TTS for thy messages", args =>
@@ -67,12 +66,12 @@ public static class Commands
             if (args.Length > 0 ? args[0] == "on" : !Settings.AutoTTS)
             {
                 Settings.AutoTTS = true;
-                chat.Receive("[green]TTS enabled.");
+                Receive("[green]TTS enabled.");
             }
             else
             {
                 Settings.AutoTTS = false;
-                chat.Receive("[red]TTS disabled.");
+                Receive("[red]TTS disabled.");
             }
         });
 
@@ -92,7 +91,7 @@ public static class Commands
         {
             if (args.Length == 0)
             {
-                chat.Receive("[red]No plushie name was provided.");
+                Receive("[red]No plushie name was provided.");
                 return;
             }
 
@@ -100,7 +99,7 @@ public static class Commands
             int type = GameAssets.Plushies.IndexOf(n => n.Contains(name));
 
             if (type == -1)
-                chat.Receive($"[red]Couldn't find a plushie named {name}.");
+                Receive($"[red]Couldn't find a plushie named {name}.");
             else
                 Entities.Items.Make(EntityType.Hakita + (byte)type, NewMovement.Instance.transform.position);
         });
@@ -109,25 +108,25 @@ public static class Commands
         {
             if (args.Length == 1 && args[0].Contains('-')) args = args[0].Split('-');
 
-            if (LobbyController.Online && !LobbyController.IsOwner) chat.Receive("[red]Only the owner of the lobby can load levels.");
+            if (LobbyController.Online && !LobbyController.IsOwner) Receive("[red]Only the owner of the lobby can load levels.");
 
             else if (args.Length >= 1 && args[0].Length >= 2 && "sandbox sbtest".Contains(args[0]))
             {
                 LoadScn("uk_construct");
-                chat.Receive("[green]Sandbox is loading...");
+                Receive("[green]Sandbox is loading...");
             }
             else if (args.Length >= 1 && args[0].Length >= 2 && "cyber grind cg".Contains(args[0]))
             {
                 LoadScn("Endless");
-                chat.Receive("[green]The Cyber Grind is loading...");
+                Receive("[green]The Cyber Grind is loading...");
             }
             else if (args.Length >= 1 && args[0].Length >= 2 && "credits museum".Contains(args[0]))
             {
                 LoadScn("CreditsMuseum2");
-                chat.Receive("[green]Museum is loading...");
+                Receive("[green]Museum is loading...");
             }
             else if (args.Length < 2)
-                chat.Receive("[red]Insufficient number of arguments.");
+                Receive("[red]Insufficient number of arguments.");
             else if
             (
                 int.TryParse(args[0], out int layer) && layer >= 0 && layer <= 8 &&
@@ -136,25 +135,25 @@ public static class Commands
             )
             {
                 LoadScn($"Level {layer}-{level}");
-                chat.Receive($"[green]Level {layer}-{level} is loading...");
+                Receive($"[green]Level {layer}-{level} is loading...");
             }
             else if (args[1] == "s" && int.TryParse(args[0], out layer) && layer >= 0 && layer <= 7 && layer != 3 && layer != 6)
             {
                 LoadScn($"Level {layer}-S");
-                chat.Receive($"[green]Level {layer}-S is loading...");
+                Receive($"[green]Level {layer}-S is loading...");
             }
             else if (args[1] == "e" && int.TryParse(args[0], out layer) && layer >= 0 && layer <= 1)
             {
                 LoadScn($"Level {layer}-E");
-                chat.Receive($"[green]Level {layer}-E is loading...");
+                Receive($"[green]Level {layer}-E is loading...");
             }
             else if (args[0] == "p" && int.TryParse(args[1], out level) && level >= 1 && level <= 2)
             {
                 LoadScn($"Level P-{level}");
-                chat.Receive($"[green]Level P-{level} is loading...");
+                Receive($"[green]Level P-{level} is loading...");
             }
             else
-                chat.Receive("[red]Couldn't parse the given values.");
+                Receive("[red]Couldn't parse the given values.");
         });
 
         Handler.Register("authors", "Display the list of all developers", args => Print
